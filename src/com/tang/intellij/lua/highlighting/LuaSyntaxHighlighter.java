13cc7ee7ee0d3d2fa748f1eb56b6434a6de5f8f5
tangzx <272669294@qq.com>
2015-11-16 00:01:56 +0800

    highlighter

---
+++ src/com/tang/intellij/lua/highlighting/LuaSyntaxHighlighter.java
@@ -0,0 +1,64 @@
+package com.tang.intellij.lua.highlighting;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
+import com.tang.intellij.lua.lexer.LuaLexerAdapter;
+import com.tang.intellij.lua.psi.LuaTokenType;
+import com.tang.intellij.lua.psi.LuaTypes;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+
+/**
+ * Created by tangzx on 2015/11/15.
+ */
+public class LuaSyntaxHighlighter extends SyntaxHighlighterBase {
+
+    public static final TokenSet KEYWORD_TOKENS = TokenSet.create(
+            LuaTypes.AND,
+            LuaTypes.BREAK,
+            LuaTypes.DO,
+            LuaTypes.ELSE,
+            LuaTypes.ELSEIF,
+            LuaTypes.END,
+            LuaTypes.FALSE,
+            LuaTypes.FOR,
+            LuaTypes.FUNCTION,
+            LuaTypes.IF,
+            LuaTypes.IN,
+            LuaTypes.LOCAL,
+            LuaTypes.NIL,
+            LuaTypes.NOT,
+            LuaTypes.OR,
+            LuaTypes.REPEAT,
+            LuaTypes.RETURN,
+            LuaTypes.THEN,
+            LuaTypes.TRUE,
+            LuaTypes.UNTIL,
+            LuaTypes.WHILE
+    );
+
+    private static final TextAttributesKey KEYWORD = createTextAttributesKey("LUA_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
+    private static final TextAttributesKey NUMBER = createTextAttributesKey("LUA_NUMBER", DefaultLanguageHighlighterColors.NUMBER);
+
+    @NotNull
+    @Override
+    public Lexer getHighlightingLexer() {
+        return new LuaLexerAdapter();
+    }
+
+    @NotNull
+    @Override
+    public TextAttributesKey[] getTokenHighlights(IElementType type) {
+        if (KEYWORD_TOKENS.contains(type))
+            return pack(KEYWORD);
+        else if (type == LuaTypes.NUMBER)
+            return pack(NUMBER);
+
+        return new TextAttributesKey[0];
+    }
+}
---
+++ src/com/tang/intellij/lua/highlighting/LuaSyntaxHighlighterFactory.java
@@ -0,0 +1,18 @@
+package com.tang.intellij.lua.highlighting;
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by tangzx on 2015/11/15.
+ */
+public class LuaSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return new LuaSyntaxHighlighter();
+    }
+}
