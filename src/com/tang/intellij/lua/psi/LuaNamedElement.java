fce1699139d72128a0f1614b7d0bf2bb6169f897
tangzx <272669294@qq.com>
2016-11-22 20:10:43 +0800

    ref

---
+++ src/com/tang/intellij/lua/psi/LuaNamedElement.java
@@ -0,0 +1,10 @@
+package com.tang.intellij.lua.psi;
+
+import com.intellij.psi.PsiNamedElement;
+
+/**
+ *
+ * Created by TangZX on 2016/11/22.
+ */
+public interface LuaNamedElement extends PsiNamedElement, LuaPsiElement {
+}
--- src/com/tang/intellij/lua/psi/LuaPsiImplUtil.java
+++ src/com/tang/intellij/lua/psi/LuaPsiImplUtil.java
@@ -1,6 +1,14 @@
 package com.tang.intellij.lua.psi;
 
+import com.intellij.ide.navigationToolbar.NavBarUpdateQueue;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.stubs.StringStubIndexExtension;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.tang.intellij.lua.reference.LuaIdentifierReference;
+
+import java.util.Optional;
 
 /**
  * LuaPsiImplUtil
@@ -18,7 +26,24 @@ public class LuaPsiImplUtil {
         return ArgType.NAME_LIST;
     }
 
-    public static PsiReference getReference(LuaIdentifier identifier) {
+    public static PsiElement setName(LuaIdentifier identifier, String name) {
         return null;
+    }
+
+    public static String getName(LuaIdentifier identifier) {
+        return identifier.getId().getText();
+    }
+
+    public static PsiReference getReference(LuaIdentifier identifier) {
+        return new LuaIdentifierReference(identifier);
+    }
+
+    public static Optional<PsiElement> resolve(LuaIdentifier identifier) {
+        PsiFile file = identifier.getContainingFile();
+        LuaLocalDef def = PsiTreeUtil.findChildOfType(file, LuaLocalDef.class);
+        if (def != null) {
+            return Optional.of((PsiElement) def);
+        }
+        return Optional.empty();
     }
 }
