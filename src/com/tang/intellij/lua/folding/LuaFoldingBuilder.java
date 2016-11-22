3fc7ff3cf9827d3081e4921867590d79833db22f
tangzx <272669294@qq.com>
2016-11-22 15:52:38 +0800

    test folding

--- src/com/tang/intellij/lua/folding/LuaFoldingBuilder.java
+++ src/com/tang/intellij/lua/folding/LuaFoldingBuilder.java
@@ -1,35 +1,49 @@
 package com.tang.intellij.lua.folding;
 
 import com.intellij.lang.ASTNode;
-import com.intellij.lang.folding.CustomFoldingBuilder;
+import com.intellij.lang.folding.FoldingBuilderEx;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.tang.intellij.lua.psi.LuaFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.tang.intellij.lua.psi.LuaLocalFuncDef;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
  * Created by tangzx
  * Date : 2015/11/16.
  */
-public class LuaFoldingBuilder extends CustomFoldingBuilder {
+public class LuaFoldingBuilder extends FoldingBuilderEx {
+    @NotNull
     @Override
-    protected void buildLanguageFoldRegions(@NotNull List<FoldingDescriptor> list,
-                                            @NotNull PsiElement psiElement,
-                                            @NotNull Document document, boolean b) {
-        //if (!(psiElement instanceof LuaFile)) return;
+    public FoldingDescriptor[] buildFoldRegions(@NotNull PsiElement psiElement, @NotNull Document document, boolean b) {
+        List<FoldingDescriptor> descriptors = new ArrayList<FoldingDescriptor>();
+        Collection<LuaLocalFuncDef> localFuncDefs = PsiTreeUtil.findChildrenOfType(psiElement, LuaLocalFuncDef.class);
+        for (final LuaLocalFuncDef funcDef : localFuncDefs) {
+            descriptors.add(new FoldingDescriptor(funcDef, funcDef.getTextRange()) {
+                @Nullable
+                @Override
+                public String getPlaceholderText() {
+                    return "local function() ... end";
+                }
+            });
+        }
+        return descriptors.toArray(new FoldingDescriptor[descriptors.size()]);
     }
 
+    @Nullable
     @Override
-    protected String getLanguagePlaceholderText(@NotNull ASTNode astNode, @NotNull TextRange textRange) {
+    public String getPlaceholderText(@NotNull ASTNode astNode) {
         return null;
     }
 
     @Override
-    protected boolean isRegionCollapsedByDefault(@NotNull ASTNode astNode) {
-        return false;
+    public boolean isCollapsedByDefault(@NotNull ASTNode astNode) {
+        return true;
     }
 }
