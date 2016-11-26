1c90accf5ed31306980795b8b207d0fc390ec986
tangzx <272669294@qq.com>
2016-11-26 22:45:02 +0800

    optimize code

--- src/com/tang/intellij/lua/annotator/LuaAnnotator.java
+++ src/com/tang/intellij/lua/annotator/LuaAnnotator.java
@@ -5,40 +5,69 @@ import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.psi.PsiElement;
-import com.tang.intellij.lua.doc.psi.LuaDocClassName;
-import com.tang.intellij.lua.doc.psi.LuaDocClassNameRef;
-import com.tang.intellij.lua.doc.psi.LuaDocTagName;
+import com.tang.intellij.lua.doc.psi.*;
 import com.tang.intellij.lua.highlighting.LuaHighlightingData;
-import com.tang.intellij.lua.psi.LuaFuncName;
-import com.tang.intellij.lua.psi.LuaGlobalFuncDef;
+import com.tang.intellij.lua.psi.*;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * LuaAnnotator
  * Created by TangZX on 2016/11/22.
  */
-public class LuaAnnotator implements Annotator {
+public class LuaAnnotator extends LuaVisitor implements Annotator {
+    private AnnotationHolder myHolder;
+    private LuaElementVisitor luaVisitor = new LuaElementVisitor();
+    private LuaDocElementVisitor docVisitor = new LuaDocElementVisitor();
+
     @Override
     public void annotate(@NotNull PsiElement psiElement, @NotNull AnnotationHolder annotationHolder) {
-        if (psiElement instanceof LuaDocTagName) {
-            Annotation annotation = annotationHolder.createInfoAnnotation(psiElement, null);
-            annotation.setTextAttributes(DefaultLanguageHighlighterColors.DOC_COMMENT_TAG);
+        myHolder = annotationHolder;
+        if (psiElement instanceof LuaDocElementType) {
+            psiElement.accept(docVisitor);
         }
-        else if (psiElement instanceof LuaDocClassName) {
-            Annotation annotation = annotationHolder.createInfoAnnotation(psiElement, null);
-            annotation.setTextAttributes(DefaultLanguageHighlighterColors.CLASS_NAME);
+        else if (psiElement instanceof LuaPsiElement) {
+            psiElement.accept(luaVisitor);
         }
-        else if (psiElement instanceof LuaDocClassNameRef) {
-            Annotation annotation = annotationHolder.createInfoAnnotation(psiElement, null);
-            annotation.setTextAttributes(DefaultLanguageHighlighterColors.CLASS_REFERENCE);
-        }
-        else if (psiElement instanceof LuaGlobalFuncDef) {
-            LuaGlobalFuncDef def = (LuaGlobalFuncDef)psiElement;
-            LuaFuncName name = def.getFuncName();
+        myHolder = null;
+    }
+
+    class LuaElementVisitor extends LuaVisitor {
+        @Override
+        public void visitLocalFuncDef(@NotNull LuaLocalFuncDef o) {
+            LuaNameDef name = o.getNameDef();
             if (name != null) {
-                Annotation annotation = annotationHolder.createInfoAnnotation(name, null);
+                Annotation annotation = myHolder.createInfoAnnotation(name, null);
+                annotation.setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+            }
+        }
+
+        @Override
+        public void visitGlobalFuncDef(@NotNull LuaGlobalFuncDef o) {
+            LuaFuncName name = o.getFuncName();
+            if (name != null) {
+                Annotation annotation = myHolder.createInfoAnnotation(name, null);
                 annotation.setTextAttributes(LuaHighlightingData.FIELD);
             }
+        }
+    }
+
+    class LuaDocElementVisitor extends LuaDocVisitor {
+        @Override
+        public void visitTagName(@NotNull LuaDocTagName o) {
+            Annotation annotation = myHolder.createInfoAnnotation(o, null);
+            annotation.setTextAttributes(DefaultLanguageHighlighterColors.DOC_COMMENT_TAG);
+        }
+
+        @Override
+        public void visitClassName(@NotNull LuaDocClassName o) {
+            Annotation annotation = myHolder.createInfoAnnotation(o, null);
+            annotation.setTextAttributes(DefaultLanguageHighlighterColors.CLASS_NAME);
+        }
+
+        @Override
+        public void visitClassNameRef(@NotNull LuaDocClassNameRef o) {
+            Annotation annotation = myHolder.createInfoAnnotation(o, null);
+            annotation.setTextAttributes(DefaultLanguageHighlighterColors.CLASS_REFERENCE);
         }
     }
 }
