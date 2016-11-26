5cd0ed2fcd4a3df4e55019bc93825f2867f9e253
tangzx <272669294@qq.com>
2016-11-26 14:43:52 +0800

    resolveInFuncBody

--- src/com/tang/intellij/lua/psi/LuaPsiResolveUtil.java
+++ src/com/tang/intellij/lua/psi/LuaPsiResolveUtil.java
@@ -27,21 +27,36 @@ public class LuaPsiResolveUtil {
             }
             else if (curr instanceof LuaLocalFuncDef) {
                 LuaLocalFuncDef localFuncDef = (LuaLocalFuncDef) curr;
-                if (refName.equals(localFuncDef.getNameDef().getText()))
+                LuaNameDef funcName = localFuncDef.getNameDef();
+                //名字部分
+                if (funcName != null && refName.equals(funcName.getText()))
                     return localFuncDef.getNameDef();
-
-                PsiElement result = resolveInParList(localFuncDef.getFuncBody().getParList(), refName);
+                //参数部分
+                PsiElement result = resolveInFuncBody(localFuncDef.getFuncBody(), refName);
+                if (result != null) return result;
+            }
+            else if (curr instanceof LuaGlobalFuncDef) {
+                //参数部分
+                LuaGlobalFuncDef globalFuncDef = (LuaGlobalFuncDef) curr;
+                PsiElement result = resolveInFuncBody(globalFuncDef.getFuncBody(), refName);
                 if (result != null) return result;
             }
         } while (!(curr instanceof PsiFile));
         return null;
     }
 
+    static PsiElement resolveInFuncBody(LuaFuncBody body, String searchName) {
+        if (body == null) return null;
+        return resolveInParList(body.getParList(), searchName);
+    }
+
     static PsiElement resolveInParList(LuaParList parList, String searchName) {
+        if (parList == null) return null;
         return resolveInNameList(parList.getNameList(), searchName);
     }
 
     static PsiElement resolveInNameList(@NotNull LuaNameList nameList, String searchName) {
+        if (nameList == null) return null;
         for (LuaNameDef nameDef : nameList.getNameDefList()) {
             if (nameDef.getName().equals(searchName)) {
                 return nameDef;
