8bdec21f7e9d6bb063f939fd6909300881daf5f1
tangzx <272669294@qq.com>
2016-11-29 14:49:16 +0800

    del useless

--- src/com/tang/intellij/lua/psi/LuaPsiScope.java
+++
@@ -1,8 +0,0 @@
-package com.tang.intellij.lua.psi;
-
-/**
- * 范围类型，如 do end, for end, function() end
- * Created by TangZX on 2016/11/24.
- */
-public interface LuaPsiScope extends LuaPsiElement {
-}
