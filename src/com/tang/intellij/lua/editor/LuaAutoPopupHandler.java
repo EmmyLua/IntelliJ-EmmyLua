75548cb45b6b58226c450a21c9268854ecfbfa48
tangzx <272669294@qq.com>
2016-12-02 19:15:39 +0800

    function() <caret> end 自动加上end

--- src/com/tang/intellij/lua/editor/LuaAutoPopupHandler.java
+++ src/com/tang/intellij/lua/editor/LuaAutoPopupHandler.java
@@ -1,24 +1,35 @@
 package com.tang.intellij.lua.editor;
 
-import com.intellij.codeInsight.AutoPopupController;
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.tang.intellij.lua.psi.LuaFile;
+import com.tang.intellij.lua.psi.LuaFuncBody;
+import org.jetbrains.annotations.NotNull;
 
 /**
  *
  * Created by TangZX on 2016/11/28.
  */
 public class LuaAutoPopupHandler extends TypedHandlerDelegate {
+
     @Override
-    public Result checkAutoPopup(char charTyped, Project project, Editor editor, PsiFile file) {
+    public Result charTyped(char charTyped, Project project, @NotNull Editor editor, @NotNull PsiFile file) {
         if (!(file instanceof LuaFile)) return Result.CONTINUE;
 
-        if (charTyped == ':' || charTyped == '.' || charTyped == ' ' || charTyped == '@') {
-            AutoPopupController.getInstance(project).autoPopupMemberLookup(editor, null);
-            return Result.STOP;
+        // function() <caret> end 自动加上end
+        if (charTyped == ')') {
+            int pos = editor.getCaretModel().getOffset();
+            PsiElement element = file.findElementAt(pos - 1);
+            if (element != null) {
+                LuaFuncBody parent = (LuaFuncBody) element.getParent();
+                if (parent != null) {
+                    editor.getDocument().insertString(pos,"  end");
+                    editor.getCaretModel().moveToOffset(pos + 1);
+                }
+            }
         }
 
         return Result.CONTINUE;
