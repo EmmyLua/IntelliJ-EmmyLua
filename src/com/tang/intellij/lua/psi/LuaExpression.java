7ef66b2ef385f206ded11e150711a8c28adc1692
tangzx <272669294@qq.com>
2016-12-02 16:26:34 +0800

    LuaExpression

---
+++ src/com/tang/intellij/lua/psi/LuaExpression.java
@@ -0,0 +1,13 @@
+package com.tang.intellij.lua.psi;
+
+import com.tang.intellij.lua.doc.psi.LuaDocClassDef;
+
+/**
+ * 表达式，可以推算计算后的数据类型
+ * Created by TangZX on 2016/12/2.
+ */
+public interface LuaExpression extends LuaPsiElement {
+
+    // 表达式计算后的结果推算
+    LuaDocClassDef guessType();
+}
