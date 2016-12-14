2dade7fa9e0a30cfd91431a5b24b5e144bcede82
tangzx <272669294@qq.com>
2016-12-14 10:24:04 +0800

    StructureView with sorters

--- src/main/java/com/tang/intellij/lua/editor/LuaStructureViewFactory.java
+++ src/main/java/com/tang/intellij/lua/editor/LuaStructureViewFactory.java
@@ -1,13 +1,23 @@
 package com.tang.intellij.lua.editor;
 
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.IdeBundle;
 import com.intellij.ide.structureView.*;
+import com.intellij.ide.util.treeView.smartTree.ActionPresentation;
+import com.intellij.ide.util.treeView.smartTree.ActionPresentationData;
+import com.intellij.ide.util.treeView.smartTree.Sorter;
+import com.intellij.ide.util.treeView.smartTree.SorterUtil;
 import com.intellij.lang.PsiStructureViewFactory;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
+import com.tang.intellij.lua.editor.structure.LuaClassFieldTreeElement;
+import com.tang.intellij.lua.editor.structure.LuaClassMethodTreeElement;
 import com.tang.intellij.lua.editor.structure.LuaFileTreeElement;
 import com.tang.intellij.lua.psi.LuaFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Comparator;
 
 /**
  * Structure View
@@ -22,15 +32,16 @@ public class LuaStructureViewFactory implements PsiStructureViewFactory {
             @NotNull
             @Override
             public StructureViewModel createStructureViewModel(@Nullable Editor editor) {
-                return new SimpleStructureViewModel(psiFile);
+                return new LuaStructureViewModel(psiFile);
             }
         };
     }
 
-    class SimpleStructureViewModel extends StructureViewModelBase implements StructureViewModel.ElementInfoProvider {
+    class LuaStructureViewModel extends StructureViewModelBase implements StructureViewModel.ElementInfoProvider {
 
-        public SimpleStructureViewModel(@NotNull PsiFile psiFile) {
+        public LuaStructureViewModel(@NotNull PsiFile psiFile) {
             super(psiFile, new LuaFileTreeElement((LuaFile) psiFile));
+            withSorters(new LuaAlphaSorter());
         }
 
         @Override
@@ -41,6 +52,43 @@ public class LuaStructureViewFactory implements PsiStructureViewFactory {
         @Override
         public boolean isAlwaysLeaf(StructureViewTreeElement structureViewTreeElement) {
             return false;
+        }
+    }
+
+    /**
+     * 字母alpha排序，但field排在method前面
+     */
+    class LuaAlphaSorter implements Sorter {
+
+        @Override
+        public Comparator getComparator() {
+            return (o1, o2) -> {
+                if (o1 instanceof LuaClassFieldTreeElement && o2 instanceof LuaClassMethodTreeElement)
+                    return -1;
+                else if (o1 instanceof LuaClassMethodTreeElement && o2 instanceof LuaClassFieldTreeElement)
+                    return 1;
+
+                String s1 = SorterUtil.getStringPresentation(o1);
+                String s2 = SorterUtil.getStringPresentation(o2);
+                return s1.compareToIgnoreCase(s2);
+            };
+        }
+
+        @Override
+        public boolean isVisible() {
+            return true;
+        }
+
+        @NotNull
+        @Override
+        public ActionPresentation getPresentation() {
+            return new ActionPresentationData(IdeBundle.message("action.sort.alphabetically"), IdeBundle.message("action.sort.alphabetically"), AllIcons.ObjectBrowser.Sorted);
+        }
+
+        @NotNull
+        @Override
+        public String getName() {
+            return "Alpha Sorter";
         }
     }
 }
