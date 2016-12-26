3475f19a29864fac4b2cd780037cd45dd15e2c36
tangzx <272669294@qq.com>
2016-12-26 14:41:09 +0800

    usages

--- src/main/java/com/tang/intellij/lua/comment/psi/LuaDocNamedElement.java
+++ src/main/java/com/tang/intellij/lua/comment/psi/LuaDocNamedElement.java
@@ -1,10 +1,10 @@
 package com.tang.intellij.lua.comment.psi;
 
-import com.intellij.psi.PsiNamedElement;
+import com.tang.intellij.lua.psi.LuaNamedElement;
 
 /**
  *
  * Created by TangZX on 2016/11/29.
  */
-public interface LuaDocNamedElement extends LuaDocPsiElement, PsiNamedElement {
+public interface LuaDocNamedElement extends LuaDocPsiElement, LuaNamedElement {
 }
--- src/main/java/com/tang/intellij/lua/usages/LuaFindUsagesProvider.java
+++ src/main/java/com/tang/intellij/lua/usages/LuaFindUsagesProvider.java
@@ -5,6 +5,9 @@ import com.intellij.lang.cacheBuilder.WordsScanner;
 import com.intellij.lang.findUsages.FindUsagesProvider;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.TokenSet;
+import com.tang.intellij.lua.comment.psi.LuaDocClassName;
+import com.tang.intellij.lua.comment.psi.LuaDocFieldNameDef;
+import com.tang.intellij.lua.lang.LuaParserDefinition;
 import com.tang.intellij.lua.lexer.LuaLexerAdapter;
 import com.tang.intellij.lua.psi.*;
 import org.jetbrains.annotations.NotNull;
@@ -20,13 +23,13 @@ public class LuaFindUsagesProvider implements FindUsagesProvider {
     public WordsScanner getWordsScanner() {
         return new DefaultWordsScanner(new LuaLexerAdapter(),
                 TokenSet.create(),
-                TokenSet.create(),
-                TokenSet.create());
+                LuaParserDefinition.COMMENTS,
+                LuaParserDefinition.STRINGS);
     }
 
     @Override
     public boolean canFindUsagesFor(@NotNull PsiElement psiElement) {
-        return psiElement instanceof LuaName;
+        return psiElement instanceof LuaNamedElement;
     }
 
     @Nullable
@@ -38,6 +41,10 @@ public class LuaFindUsagesProvider implements FindUsagesProvider {
     @NotNull
     @Override
     public String getType(@NotNull PsiElement psiElement) {
+        if (psiElement instanceof LuaDocClassName)
+            return "Class";
+        if (psiElement instanceof LuaDocFieldNameDef)
+            return "Class Field";
         if (psiElement instanceof LuaGlobalFuncNameDef)
             return "Global Function";
         if (psiElement instanceof LuaLocalFuncNameDef)
@@ -52,15 +59,18 @@ public class LuaFindUsagesProvider implements FindUsagesProvider {
     @NotNull
     @Override
     public String getDescriptiveName(@NotNull PsiElement psiElement) {
-        if (psiElement instanceof LuaName)
-            return ((LuaName)psiElement).getName();
-        else
-            return "";
+        if (psiElement instanceof LuaNamedElement) {
+            LuaNamedElement namedElement = (LuaNamedElement) psiElement;
+            String name = namedElement.getName();
+            if (name != null)
+                return name;
+        }
+        return "";
     }
 
     @NotNull
     @Override
     public String getNodeText(@NotNull PsiElement psiElement, boolean b) {
-        return ((LuaName)psiElement).getName();
+        return getDescriptiveName(psiElement);
     }
 }
