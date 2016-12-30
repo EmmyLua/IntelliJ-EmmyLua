0296c82d0115a47a78ff0160593d484f375d5983
tangzx <272669294@qq.com>
2016-12-31 00:58:29 +0800

    breakpoint

--- src/main/java/com/tang/intellij/lua/debugger/LuaDebugProcess.java
+++ src/main/java/com/tang/intellij/lua/debugger/LuaDebugProcess.java
@@ -7,8 +7,10 @@ import com.intellij.xdebugger.breakpoints.XBreakpointHandler;
 import com.intellij.xdebugger.breakpoints.XBreakpointProperties;
 import com.intellij.xdebugger.breakpoints.XLineBreakpoint;
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider;
+import com.intellij.xdebugger.frame.XSuspendContext;
 import com.tang.intellij.lua.debugger.mobdebug.MobServer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 
@@ -20,6 +22,7 @@ public class LuaDebugProcess extends XDebugProcess implements MobServer.Listener
 
     private LuaDebuggerEditorsProvider editorsProvider;
     private MobServer mobServer;
+    private XLineBreakpoint<XBreakpointProperties> breakpoint;
 
     LuaDebugProcess(@NotNull XDebugSession session) {
         super(session);
@@ -45,6 +48,26 @@ public class LuaDebugProcess extends XDebugProcess implements MobServer.Listener
         }
     }
 
+    @Override
+    public void resume(@Nullable XSuspendContext context) {
+        mobServer.addCommand("RUN");
+    }
+
+    @Override
+    public void startStepOver(@Nullable XSuspendContext context) {
+        mobServer.addCommand("OVER");
+    }
+
+    @Override
+    public void startStepInto(@Nullable XSuspendContext context) {
+        mobServer.addCommand("STEP");
+    }
+
+    @Override
+    public void startStepOut(@Nullable XSuspendContext context) {
+        mobServer.addCommand("OUT");
+    }
+
     @NotNull
     @Override
     public XBreakpointHandler<?>[] getBreakpointHandlers() {
@@ -52,6 +75,7 @@ public class LuaDebugProcess extends XDebugProcess implements MobServer.Listener
     }
 
     void addBreakpoint(XLineBreakpoint<XBreakpointProperties> breakpoint) {
+        this.breakpoint = breakpoint;
         XSourcePosition sourcePosition = breakpoint.getSourcePosition();
         if (sourcePosition != null) {
             String shortFilePath = breakpoint.getShortFilePath();
@@ -69,6 +93,10 @@ public class LuaDebugProcess extends XDebugProcess implements MobServer.Listener
 
     @Override
     public void handleResp(int code, String[] params) {
-
+        switch (code) {
+            case 202:
+                getSession().breakpointReached(breakpoint, "test", new LuaSuspendContext());
+                break;
+        }
     }
 }
---
+++ src/main/java/com/tang/intellij/lua/debugger/LuaSuspendContext.java
@@ -0,0 +1,11 @@
+package com.tang.intellij.lua.debugger;
+
+import com.intellij.xdebugger.frame.XSuspendContext;
+
+/**
+ *
+ * Created by tangzx on 2016/12/31.
+ */
+public class LuaSuspendContext extends XSuspendContext {
+
+}
