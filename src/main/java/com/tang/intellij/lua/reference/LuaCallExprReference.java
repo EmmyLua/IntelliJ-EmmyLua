6efd5cda7dfddb3ad4f5b125ba1e6118835535a0
tangzx <272669294@qq.com>
2017-02-09 21:13:42 +0800

    bug fix

--- src/main/java/com/tang/intellij/lua/reference/LuaCallExprReference.java
+++ src/main/java/com/tang/intellij/lua/reference/LuaCallExprReference.java
@@ -16,7 +16,6 @@
 
 package com.tang.intellij.lua.reference;
 
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReferenceBase;
@@ -24,6 +23,7 @@ import com.intellij.util.IncorrectOperationException;
 import com.tang.intellij.lua.lang.type.LuaType;
 import com.tang.intellij.lua.lang.type.LuaTypeSet;
 import com.tang.intellij.lua.psi.LuaCallExpr;
+import com.tang.intellij.lua.psi.LuaClassField;
 import com.tang.intellij.lua.psi.LuaClassMethodDef;
 import com.tang.intellij.lua.psi.LuaElementFactory;
 import com.tang.intellij.lua.search.SearchContext;
@@ -68,15 +68,19 @@ public class LuaCallExprReference extends PsiReferenceBase<LuaCallExpr> {
     public PsiElement resolve() {
         PsiElement id = expr.getId();
         if (id != null) {
-            LuaTypeSet typeSet = expr.guessPrefixType(new SearchContext(myElement.getProject()));
+            SearchContext context = new SearchContext(myElement.getProject());
+            LuaTypeSet typeSet = expr.guessPrefixType(context);
             if (typeSet != null) {
                 String methodName = id.getText();
-                Project project = expr.getProject();
                 for (LuaType luaType : typeSet.getTypes()) {
-                    LuaClassMethodDef def = luaType.findMethod(methodName, new SearchContext(project));
-                    if (def != null) {
+                    LuaClassMethodDef def = luaType.findMethod(methodName, context);
+                    if (def != null)
                         return def;
-                    }
+
+                    //也有可能是字段
+                    LuaClassField field = luaType.findField(methodName, context);
+                    if (field != null)
+                        return field;
                 }
             }
         }
