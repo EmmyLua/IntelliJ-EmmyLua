506e4f783d172e6872e95bf54b787b7946e9e708
tangzx <272669294@qq.com>
2017-02-19 22:26:54 +0800

    smart del doc dash

--- src/main/java/com/tang/intellij/lua/codeInsight/LuaBackspaceHandlerDelegate.java
+++ src/main/java/com/tang/intellij/lua/codeInsight/LuaBackspaceHandlerDelegate.java
@@ -14,14 +14,17 @@
  * limitations under the License.
  */
 
-package com.tang.intellij.lua.editor;
+package com.tang.intellij.lua.codeInsight;
 
 import com.intellij.codeInsight.editorActions.BackspaceHandlerDelegate;
+import com.intellij.lang.ASTNode;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.tang.intellij.lua.comment.psi.LuaDocTypes;
+import com.tang.intellij.lua.comment.psi.api.LuaComment;
 
 /**
  *
@@ -39,11 +42,19 @@ public class LuaBackspaceHandlerDelegate extends BackspaceHandlerDelegate {
             int offset = editor.getCaretModel().getOffset();
             PsiElement element = psiFile.findElementAt(offset);
             if (element != null) {
-                IElementType type = element.getNode().getElementType();
+                ASTNode node = element.getNode();
+                IElementType type = node.getElementType();
                 if (type == LuaDocTypes.DASHES) {
-                    int start = element.getTextOffset() - 1;
-                    int end = element.getTextOffset() + element.getTextLength();
-                    if (offset == end - 1) { //在 --- 最后面删的
+                    int end = node.getStartOffset() + node.getTextLength();
+                    int start = node.getStartOffset();
+                    if (offset == end - 1 && node.getTextLength() == 3) { //确保是在 --- 后面删的
+
+                        LuaComment comment = PsiTreeUtil.getParentOfType(element, LuaComment.class);
+                        assert comment != null;
+
+                        if (comment.getNode().getStartOffset() < start) //如果有两行以上的 --- 则把换行符一起删了
+                            start = start - 1;
+
                         editor.getDocument().deleteString(start, offset);
                         editor.getCaretModel().moveToOffset(start);
                     }
