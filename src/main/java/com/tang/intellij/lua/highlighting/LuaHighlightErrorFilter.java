d5e09bf7c9058a486cf28e4620662067f4f115d8
tangzx <272669294@qq.com>
2017-02-19 23:11:39 +0800

    disable error highlight for doc elements

---
+++ src/main/java/com/tang/intellij/lua/highlighting/LuaHighlightErrorFilter.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.highlighting;
+
+import com.intellij.codeInsight.highlighting.HighlightErrorFilter;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiErrorElement;
+import com.tang.intellij.lua.comment.psi.LuaDocPsiElement;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * disable error highlight for doc elements
+ * Created by tangzx on 2017/2/19.
+ */
+public class LuaHighlightErrorFilter extends HighlightErrorFilter {
+    @Override
+    public boolean shouldHighlightErrorElement(@NotNull PsiErrorElement psiErrorElement) {
+        PsiElement parent = psiErrorElement.getParent();
+        return parent == null || !(parent instanceof LuaDocPsiElement);
+    }
+}
