9bfbad1860f047a6203edf66e19fd38275cf1ef8
tangzx <272669294@qq.com>
2017-02-19 22:51:51 +0800

    quote handler

---
+++ src/main/java/com/tang/intellij/lua/codeInsight/editorActions/LuaQuoteHandler.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.codeInsight.editorActions;
+
+import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler;
+import com.intellij.psi.tree.TokenSet;
+import com.tang.intellij.lua.psi.LuaTypes;
+
+/**
+ * LuaQuoteHandler
+ * Created by tangzx on 2017/2/19.
+ */
+public class LuaQuoteHandler extends SimpleTokenSetQuoteHandler {
+    public LuaQuoteHandler() {
+        super(TokenSet.create(LuaTypes.STRING));
+    }
+}
