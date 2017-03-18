ed93ff0cd292036244374a40d8349db5e26e9439
tangzx <272669294@qq.com>
2017-03-18 23:34:53 +0800

    function exit points highlighting

---
+++ src/main/java/com/tang/intellij/lua/codeInsight/highlighting/LuaHighlightUsagesHandlerFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.codeInsight.highlighting;
+
+import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase;
+import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerFactoryBase;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.tang.intellij.lua.psi.LuaReturnStat;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ *
+ * Created by tangzx on 2017/3/18.
+ */
+public class LuaHighlightUsagesHandlerFactory extends HighlightUsagesHandlerFactoryBase {
+    @Nullable
+    @Override
+    public HighlightUsagesHandlerBase createHighlightUsagesHandler(@NotNull Editor editor,
+                                                                   @NotNull PsiFile psiFile,
+                                                                   @NotNull PsiElement psiElement) {
+        LuaReturnStat returnStat = PsiTreeUtil.getParentOfType(psiElement, LuaReturnStat.class);
+        if (returnStat != null) {
+            return new LuaHighlightExitPointsHandler(editor, psiFile, returnStat);
+        }
+        return null;
+    }
+}
