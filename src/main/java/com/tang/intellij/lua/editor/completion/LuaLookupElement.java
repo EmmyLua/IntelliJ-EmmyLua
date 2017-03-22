22cf231d2e761a5cd1e4b0535e64f8dc15b70a28
tangzx <272669294@qq.com>
2017-03-22 20:51:45 +0800

    lookup element

--- src/main/java/com/tang/intellij/lua/editor/completion/LuaLookupElement.java
+++ src/main/java/com/tang/intellij/lua/editor/completion/LuaLookupElement.java
@@ -16,23 +16,116 @@
 
 package com.tang.intellij.lua.editor.completion;
 
+import com.intellij.codeInsight.completion.BasicInsertHandler;
+import com.intellij.codeInsight.completion.InsertHandler;
+import com.intellij.codeInsight.completion.InsertionContext;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.codeInsight.lookup.LookupElementPresentation;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.tang.intellij.lua.lang.LuaIcons;
 import com.tang.intellij.lua.stubs.index.LuaClassIndex;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
 import java.util.Collection;
 
 /**
  * LuaLookupElement
  * Created by TangZX on 2016/12/16.
  */
-public class LuaLookupElement {
+public class LuaLookupElement extends LookupElement implements Comparable<LookupElement> {
     public static void fillTypes(Project project, Collection<LookupElement> results) {
         Collection<String> collection = LuaClassIndex.getInstance().getAllKeys(project);
         collection.forEach(className -> {
             results.add(LookupElementBuilder.create(className).withIcon(LuaIcons.CLASS));
         });
+    }
+
+
+    protected final String myLookupString;
+    protected final String myTypeText;
+    protected final boolean isBold;
+    protected final Icon myIcon;
+    private final Icon myTypeIcon;
+    protected final String myTailText;
+    protected InsertHandler<LookupElement> myHandler;
+
+    public LuaLookupElement(@NotNull final String lookupString,
+                            @Nullable final String tailText,
+                            @Nullable final String typeText, final boolean bold,
+                            @Nullable final Icon icon,
+                            @Nullable final Icon typeIcon,
+                            @NotNull final InsertHandler<LookupElement> handler) {
+        myLookupString = lookupString;
+        myTailText = tailText;
+        myTypeText = typeText;
+        isBold = bold;
+        myIcon = icon;
+        myTypeIcon = typeIcon;
+        myHandler = handler;
+    }
+
+    public LuaLookupElement(@NotNull final String lookupString,
+                            @Nullable final String tailText,
+                            @Nullable final String typeText, final boolean bold,
+                            @Nullable final Icon icon,
+                            @Nullable final Icon typeIcon) {
+        this(lookupString, tailText, typeText, bold, icon, typeIcon, new BasicInsertHandler<>());
+    }
+
+    public LuaLookupElement(
+            @NotNull final String lookupString,
+            final boolean bold,
+            @Nullable final Icon icon) {
+        this(lookupString, null, null, bold, icon, null, new BasicInsertHandler<>());
+    }
+
+    @NotNull
+    public String getLookupString() {
+        return myLookupString;
+    }
+
+    @Nullable
+    public String getTailText() {
+        return !StringUtil.isEmpty(myTailText) ? myTailText : null;
+    }
+
+    @Nullable
+    protected String getTypeText() {
+        return !StringUtil.isEmpty(myTypeText) ? myTypeText : null;
+    }
+
+    public Icon getIcon() {
+        return myIcon;
+    }
+
+
+    public Icon getTypeIcon() {
+        return myTypeIcon;
+    }
+
+    @Override
+    public void handleInsert(InsertionContext context) {
+        myHandler.handleInsert(context, this);
+    }
+
+    public void setHandler(InsertHandler<LookupElement> handler) {
+        myHandler = handler;
+    }
+
+    @Override
+    public void renderElement(LookupElementPresentation presentation) {
+        presentation.setItemText(getLookupString());
+        presentation.setItemTextBold(isBold);
+        presentation.setTailText(getTailText());
+        presentation.setTypeText(getTypeText(), getTypeIcon());
+        presentation.setIcon(getIcon());
+    }
+
+    public int compareTo(final LookupElement o) {
+        return myLookupString.compareTo(o.getLookupString());
     }
 }
