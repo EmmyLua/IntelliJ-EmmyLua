f84d60be9652ed93d2e13f22dad8a852e91fba23
tangzx <272669294@qq.com>
2017-03-23 00:10:08 +0800

    complex doc flex

--- src/main/java/com/tang/intellij/lua/comment/lexer/LuaDocLexerAdapter.java
+++ src/main/java/com/tang/intellij/lua/comment/lexer/LuaDocLexerAdapter.java
@@ -16,10 +16,110 @@
 
 package com.tang.intellij.lua.comment.lexer;
 
-import com.intellij.lexer.FlexAdapter;
+import com.intellij.lexer.LexerBase;
+import com.intellij.lexer.MergingLexerAdapter;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
+import com.tang.intellij.lua.comment.psi.LuaDocTypes;
+import org.jetbrains.annotations.NotNull;
 
-public class LuaDocLexerAdapter extends FlexAdapter {
+import java.io.IOException;
+
+public class LuaDocLexerAdapter extends MergingLexerAdapter {
     public LuaDocLexerAdapter() {
-        super(new _LuaDocLexer());
+        super(new DashStripperLexer(new _LuaDocLexer()), TokenSet.create(LuaDocTypes.STRING));
+    }
+
+    private static class DashStripperLexer extends LexerBase {
+
+        private _LuaDocLexer myFlexLexer;
+        private CharSequence myBuffer;
+        private int myBufferIndex;
+        private int myBufferEndOffset;
+        private IElementType myTokenType;
+        private int myTokenEndOffset;
+        private int myState;
+
+        DashStripperLexer(_LuaDocLexer luaDocLexer) {
+            myFlexLexer = luaDocLexer;
+        }
+
+        @Override
+        public final void start(@NotNull CharSequence buffer, int startOffset, int endOffset, int initialState) {
+            myBuffer = buffer;
+            myBufferIndex = startOffset;
+            myBufferEndOffset = endOffset;
+            myTokenType = null;
+            myTokenEndOffset = startOffset;
+            myFlexLexer.reset(myBuffer, startOffset, endOffset, initialState);
+        }
+
+        @Override
+        public int getState() {
+            return getTokenStart() == 0 ? 0 : myState;
+        }
+
+        @Override
+        @NotNull
+        public CharSequence getBufferSequence() {
+            return myBuffer;
+        }
+
+        @Override
+        public int getBufferEnd() {
+            return myBufferEndOffset;
+        }
+
+        @Override
+        public final IElementType getTokenType() {
+            locateToken();
+            return myTokenType;
+        }
+
+        @Override
+        public final int getTokenStart() {
+            locateToken();
+            return myBufferIndex;
+        }
+
+        @Override
+        public final int getTokenEnd() {
+            locateToken();
+            return myTokenEndOffset;
+        }
+
+        @Override
+        public final void advance() {
+            locateToken();
+            myTokenType = null;
+        }
+
+        final void locateToken() {
+            if (myTokenType != null) return;
+            _locateToken();
+        }
+
+        private void _locateToken() {
+            if (myTokenEndOffset == myBufferEndOffset) {
+                myTokenType = null;
+                myBufferIndex = myBufferEndOffset;
+                return;
+            }
+
+            myBufferIndex = myTokenEndOffset;
+
+            flexLocateToken();
+        }
+
+        private void flexLocateToken() {
+            try {
+                myState = myFlexLexer.yystate();
+                //myFlexLexer.goTo(myBufferIndex);
+                myTokenType = myFlexLexer.advance();
+                myTokenEndOffset = myFlexLexer.getTokenEnd();
+            } catch (IOException e) {
+                // Can't be
+            }
+        }
     }
 }
