d1d10f73f459b1732724cac341f02015528ccbfd
tangzx <272669294@qq.com>
2017-03-30 21:19:01 +0800

    optimize line marker

---
+++ src/main/java/com/tang/intellij/lua/codeInsight/LuaLineMarkerNavigator.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.codeInsight;
+
+import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
+import com.intellij.codeInsight.daemon.impl.PsiElementListNavigator;
+import com.intellij.ide.util.DefaultPsiElementCellRenderer;
+import com.intellij.psi.NavigatablePsiElement;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.Query;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * Created by tangzx on 2017/3/30.
+ */
+public abstract class LuaLineMarkerNavigator<T extends PsiElement> implements GutterIconNavigationHandler<T> {
+    @Override
+    public void navigate(MouseEvent mouseEvent, T t) {
+        final List<NavigatablePsiElement> navElements = new ArrayList<>();
+        Query<T> search = search(t);
+        search.forEach(t1 -> { navElements.add((NavigatablePsiElement) t1); });
+        PsiElementListNavigator.openTargets(mouseEvent,
+                navElements.toArray(new NavigatablePsiElement[navElements.size()]),
+                getTitle(t),
+                null,
+                new DefaultPsiElementCellRenderer());
+    }
+
+    protected abstract String getTitle(T elt);
+
+    @NotNull
+    protected abstract Query<T> search(T elt);
+}
--- src/main/java/com/tang/intellij/lua/codeInsight/LuaLineMarkerProvider.java
+++ src/main/java/com/tang/intellij/lua/codeInsight/LuaLineMarkerProvider.java
@@ -16,14 +16,17 @@
 
 package com.tang.intellij.lua.codeInsight;
 
-import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
-import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
+import com.intellij.codeHighlighting.Pass;
+import com.intellij.codeInsight.daemon.LineMarkerInfo;
+import com.intellij.codeInsight.daemon.LineMarkerProvider;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.editor.markup.GutterIconRenderer;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.Function;
 import com.intellij.util.Query;
 import com.tang.intellij.lua.comment.psi.LuaDocClassDef;
 import com.tang.intellij.lua.lang.type.LuaType;
@@ -34,17 +37,56 @@ import com.tang.intellij.lua.psi.search.LuaOverridingMethodsSearch;
 import com.tang.intellij.lua.search.SearchContext;
 import com.tang.intellij.lua.stubs.index.LuaClassMethodIndex;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * line marker
  * Created by tangzx on 2016/12/11.
  */
-public class LuaLineMarkerProvider extends RelatedItemLineMarkerProvider {
+public class LuaLineMarkerProvider implements LineMarkerProvider {
 
-    @Override
-    protected void collectNavigationMarkers(@NotNull PsiElement element, Collection<? super RelatedItemLineMarkerInfo> result) {
+    private static final Function<LuaClassMethodDef, String> overridingMethodTooltipProvider = (classMethodDef) -> {
+        final StringBuilder builder = new StringBuilder("<html>Is overridden in:");
+        String name = classMethodDef.getName();
+        if (name != null) {
+        }
+        return builder.toString();
+    };
+
+    private static final LuaLineMarkerNavigator<LuaClassMethodDef> overridingMethodNavigator = new LuaLineMarkerNavigator<LuaClassMethodDef>() {
+
+        @Override
+        protected String getTitle(LuaClassMethodDef elt) {
+            return "Choose Overriding Method of " + elt.getName();
+        }
+
+        @NotNull
+        @Override
+        protected Query<LuaClassMethodDef> search(LuaClassMethodDef elt) {
+            return LuaOverridingMethodsSearch.search(elt);
+        }
+    };
+
+    private static final Function<LuaDocClassDef, String> subclassTooltipProvider = LuaDocClassDef::getName;
+
+    private static final LuaLineMarkerNavigator<LuaDocClassDef> subclassNavigator = new LuaLineMarkerNavigator<LuaDocClassDef>() {
+        @Override
+        protected String getTitle(LuaDocClassDef elt) {
+            return "Choose Subclass of " + elt.getName();
+        }
+
+        @NotNull
+        @Override
+        protected Query<LuaDocClassDef> search(LuaDocClassDef elt) {
+            Project project = elt.getProject();
+            return LuaClassInheritorsSearch.search(GlobalSearchScope.allScope(project), project, elt.getName());
+        }
+    };
+
+    private void collectNavigationMarkers(@NotNull PsiElement element, Collection<? super LineMarkerInfo> result) {
         if (element instanceof LuaClassMethodName) {
             LuaClassMethodName classMethodName = (LuaClassMethodName) element;
             LuaClassMethodDef methodDef = PsiTreeUtil.getParentOfType(element, LuaClassMethodDef.class);
@@ -54,6 +96,7 @@ public class LuaLineMarkerProvider extends RelatedItemLineMarkerProvider {
             LuaType type = methodDef.getClassType(context);
 
             //OverridingMethod
+            PsiElement classMethodNameId = classMethodName.getId();
             if (type != null) {
                 String methodName = methodDef.getName();
                 assert methodName != null;
@@ -67,7 +110,7 @@ public class LuaLineMarkerProvider extends RelatedItemLineMarkerProvider {
                                 NavigationGutterIconBuilder.create(AllIcons.Gutter.OverridingMethod)
                                         .setTargets(superMethod)
                                         .setTooltipText("Override in " + superTypeName);
-                        result.add(builder.createLineMarkerInfo(classMethodName.getId()));
+                        result.add(builder.createLineMarkerInfo(classMethodNameId));
                         break;
                     }
                     superType = superType.getSuperClass(context);
@@ -76,11 +119,14 @@ public class LuaLineMarkerProvider extends RelatedItemLineMarkerProvider {
 
             // OverridenMethod
             Query<LuaClassMethodDef> search = LuaOverridingMethodsSearch.search(methodDef);
-            Collection<LuaClassMethodDef> all = search.findAll();
-            if (!all.isEmpty()) {
-                NavigationGutterIconBuilder<PsiElement> builder =
-                        NavigationGutterIconBuilder.create(AllIcons.Gutter.OverridenMethod).setTargets(all);
-                result.add(builder.createLineMarkerInfo(classMethodName.getId()));
+            if (search.findFirst() != null) {
+                result.add(new LineMarkerInfo<>(methodDef,
+                        classMethodNameId.getTextRange(),
+                        AllIcons.Gutter.OverridenMethod,
+                        Pass.LINE_MARKERS,
+                        overridingMethodTooltipProvider,
+                        overridingMethodNavigator,
+                        GutterIconRenderer.Alignment.CENTER));
             }
         }
         else if (element instanceof LuaDocClassDef) {
@@ -88,12 +134,28 @@ public class LuaLineMarkerProvider extends RelatedItemLineMarkerProvider {
             LuaType classType = docClassDef.getClassType();
             Project project = element.getProject();
             Query<LuaDocClassDef> query = LuaClassInheritorsSearch.search(GlobalSearchScope.allScope(project), project, classType.getClassName());
-            Collection<LuaDocClassDef> all = query.findAll();
-            if (!all.isEmpty()) {
-                NavigationGutterIconBuilder<PsiElement> builder =
-                        NavigationGutterIconBuilder.create(AllIcons.Gutter.OverridenMethod).setTargets(all);
-                result.add(builder.createLineMarkerInfo(docClassDef));
+            if (query.findFirst() != null) {
+                result.add(new LineMarkerInfo<>(docClassDef,
+                        docClassDef.getTextRange(),
+                        AllIcons.Gutter.OverridenMethod,
+                        Pass.LINE_MARKERS,
+                        subclassTooltipProvider,
+                        subclassNavigator,
+                        GutterIconRenderer.Alignment.CENTER));
             }
+        }
+    }
+
+    @Nullable
+    @Override
+    public LineMarkerInfo getLineMarkerInfo(@NotNull PsiElement psiElement) {
+        return null;
+    }
+
+    @Override
+    public void collectSlowLineMarkers(@NotNull List<PsiElement> list, @NotNull Collection<LineMarkerInfo> collection) {
+        for (PsiElement element : list) {
+            collectNavigationMarkers(element, collection);
         }
     }
 }
