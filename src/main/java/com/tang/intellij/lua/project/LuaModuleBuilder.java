a3b91f7c6cbefa29e739b58d54468eabf0359e30
tangzx <love.tangzx@qq.com>
2017-06-04 19:32:10 +0800

    create project with src root

--- src/main/java/com/tang/intellij/lua/project/LuaModuleBuilder.java
+++ src/main/java/com/tang/intellij/lua/project/LuaModuleBuilder.java
@@ -16,22 +16,30 @@
 
 package com.tang.intellij.lua.project;
 
-import com.intellij.ide.util.projectWizard.ModuleBuilder;
-import com.intellij.ide.util.projectWizard.ModuleWizardStep;
-import com.intellij.ide.util.projectWizard.SdkSettingsStep;
-import com.intellij.ide.util.projectWizard.SettingsStep;
+import com.intellij.ide.util.projectWizard.*;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * lua ModuleBuilder
  * Created by tangzx on 2016/12/24.
  */
-public class LuaModuleBuilder extends ModuleBuilder {
+public class LuaModuleBuilder extends ModuleBuilder implements SourcePathsBuilder {
     private Sdk selectedSDK;
+    private List<Pair<String, String>> sourcePaths;
 
     @Override
     public void setupRootModel(ModifiableRootModel rootModel) throws ConfigurationException {
@@ -40,7 +48,22 @@ public class LuaModuleBuilder extends ModuleBuilder {
         else
             rootModel.inheritSdk();
 
-        doAddContentEntry(rootModel);
+        ContentEntry contentEntry = doAddContentEntry(rootModel);
+        if (contentEntry != null) {
+            final List<Pair<String,String>> sourcePaths = getSourcePaths();
+
+            if (sourcePaths != null) {
+                for (final Pair<String, String> sourcePath : sourcePaths) {
+                    String first = sourcePath.first;
+                    new File(first).mkdirs();
+                    final VirtualFile sourceRoot = LocalFileSystem.getInstance()
+                            .refreshAndFindFileByPath(FileUtil.toSystemIndependentName(first));
+                    if (sourceRoot != null) {
+                        contentEntry.addSourceFolder(sourceRoot, false, sourcePath.second);
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -58,5 +81,26 @@ public class LuaModuleBuilder extends ModuleBuilder {
                 LuaModuleBuilder.this.selectedSDK = myJdkComboBox.getSelectedJdk();
             }
         };
+    }
+
+    @Override
+    public List<Pair<String, String>> getSourcePaths() throws ConfigurationException {
+        if (sourcePaths == null) {
+            sourcePaths = new ArrayList<>();
+            @NonNls final String path = getContentEntryPath() + File.separator + "src";
+            new File(path).mkdirs();
+            sourcePaths.add(Pair.create(path, ""));
+        }
+        return sourcePaths;
+    }
+
+    @Override
+    public void setSourcePaths(List<Pair<String, String>> list) {
+        this.sourcePaths = list;
+    }
+
+    @Override
+    public void addSourcePath(Pair<String, String> pair) {
+        sourcePaths.add(pair);
     }
 }
