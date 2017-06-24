c0292c565d558b4875144b6662d1c91b1df32df0
tangzx <272669294@qq.com>
2017-06-25 00:19:42 +0800

    semantic highlighting

---
+++ src/main/java/com/tang/intellij/lua/codeInsight/LuaRainbowVisitor.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.codeInsight
+
+import com.intellij.codeInsight.daemon.RainbowVisitor
+import com.intellij.codeInsight.daemon.impl.HighlightVisitor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.tang.intellij.lua.comment.psi.LuaDocParamNameRef
+import com.tang.intellij.lua.highlighting.LuaHighlightingData
+import com.tang.intellij.lua.psi.*
+
+class LuaRainbowVisitor : RainbowVisitor() {
+    override fun clone(): HighlightVisitor = LuaRainbowVisitor()
+
+    override fun visit(element: PsiElement) {
+        if (element is LuaNameExpr ||
+                element is LuaNameDef ||
+                element is LuaDocParamNameRef)
+        {
+            val context = PsiTreeUtil.findFirstParent(element, { it is LuaFuncBodyOwner })
+            if (context != null) {
+                val resolve = when (element) {
+                    is LuaNameExpr -> element.reference?.resolve()
+                    else -> element
+                }
+                val name = when (resolve) {
+                    is LuaNameDef -> resolve.name
+                    else -> element.text
+                }
+                val key = when (resolve) {
+                    is LuaParamNameDef -> LuaHighlightingData.PARAMETER
+                    else -> null
+                }
+                resolve?.let {
+                    val info = getInfo(context, element, name, key)
+                    addInfo(info)
+                }
+            }
+        }
+    }
+
+    override fun suitableForFile(p0: PsiFile): Boolean = p0 is LuaFile
+}
