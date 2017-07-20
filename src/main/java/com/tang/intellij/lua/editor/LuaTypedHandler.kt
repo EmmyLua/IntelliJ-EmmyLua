95ee299b2febfcb7ace2e7da3782a871752d7eb5
tangzx <272669294@qq.com>
2017-07-21 00:22:41 +0800

    auto insert " end" when typed '(' after 'function'

---
+++ src/main/java/com/tang/intellij/lua/editor/LuaTypedHandler.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.editor
+
+import com.intellij.codeInsight.AutoPopupController
+import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFile
+import com.tang.intellij.lua.psi.LuaFuncBody
+
+/**
+
+ * Created by TangZX on 2016/11/28.
+ */
+class LuaTypedHandler : TypedHandlerDelegate() {
+
+    override fun checkAutoPopup(charTyped: Char, project: Project, editor: Editor, file: PsiFile): TypedHandlerDelegate.Result {
+        if (charTyped == ':' || charTyped == '@') {
+            AutoPopupController.getInstance(project).autoPopupMemberLookup(editor, null)
+            return TypedHandlerDelegate.Result.STOP
+        }
+        return super.checkAutoPopup(charTyped, project, editor, file)
+    }
+
+    override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile): TypedHandlerDelegate.Result {
+        // function() <caret> end 自动加上end
+        if (c == '(') {
+            PsiDocumentManager.getInstance(project).commitDocument(editor.document)
+            val pos = editor.caretModel.offset
+            val element = file.findElementAt(pos)
+            if (element != null && element.parent is LuaFuncBody) {
+                editor.document.insertString(pos + 1, " end")
+                return TypedHandlerDelegate.Result.STOP
+            }
+        }
+        return super.charTyped(c, project, editor, file)
+    }
+}
