8a61aa07056a1d1cdd1427cd8614878e4f1eae9d
tangzx <love.tangzx@qq.com>
2017-07-25 18:44:48 +0800

    template context

--- src/main/java/com/tang/intellij/lua/codeInsight/LuaCodeTemplateContextType.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/LuaCodeTemplateContextType.kt
@@ -2,7 +2,11 @@ package com.tang.intellij.lua.codeInsight
 
 import com.intellij.codeInsight.template.TemplateContextType
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.util.PsiUtilCore
 import com.tang.intellij.lua.lang.LuaFileType
+import com.tang.intellij.lua.lang.LuaLanguage
+import com.tang.intellij.lua.psi.LuaTypes
 
 /**
 
@@ -10,7 +14,16 @@ import com.tang.intellij.lua.lang.LuaFileType
  */
 class LuaCodeTemplateContextType : TemplateContextType("LUA_CODE", "Lua") {
 
-    override fun isInContext(psiFile: PsiFile, i: Int): Boolean {
-        return psiFile.fileType === LuaFileType.INSTANCE
+    override fun isInContext(file: PsiFile, offset: Int): Boolean {
+        if (PsiUtilCore.getLanguageAtOffset(file, offset).isKindOf(LuaLanguage.INSTANCE)) {
+            val element = file.findElementAt(offset)
+            if (element == null || element is PsiWhiteSpace) {
+                return false
+            }
+            if (element.node.elementType in arrayOf(LuaTypes.STRING, LuaTypes.NUMBER)) {
+                return false
+            }
+        }
+        return file.fileType === LuaFileType.INSTANCE
     }
 }
