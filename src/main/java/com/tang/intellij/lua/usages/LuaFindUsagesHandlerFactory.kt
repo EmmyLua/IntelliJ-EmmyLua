ce5940f5ac11b7231a69dc8adf77bcefd13e7854
tangzx <272669294@qq.com>
2017-08-03 20:54:16 +0800

    fix bug #22  highlight/find references overriding methods

---
+++ src/main/java/com/tang/intellij/lua/usages/LuaFindUsagesHandlerFactory.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.usages
+
+import com.intellij.find.findUsages.FindUsagesHandler
+import com.intellij.find.findUsages.FindUsagesHandlerFactory
+import com.intellij.find.findUsages.FindUsagesOptions
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import com.intellij.psi.search.SearchScope
+import com.intellij.usageView.UsageInfo
+import com.intellij.util.Processor
+import com.tang.intellij.lua.psi.LuaClassMethodDef
+import com.tang.intellij.lua.psi.search.LuaOverridingMethodsSearch
+import com.tang.intellij.lua.reference.LuaOverridingMethodReference
+
+class LuaFindUsagesHandlerFactory : FindUsagesHandlerFactory() {
+    override fun createFindUsagesHandler(element: PsiElement, forHighlightUsages: Boolean): FindUsagesHandler? {
+        if (element is LuaClassMethodDef)
+            return FindMethodUsagesHandler(element)
+        return null
+    }
+
+    override fun canFindUsages(element: PsiElement): Boolean {
+        return element is LuaClassMethodDef
+    }
+}
+
+/**
+ * 查找方法的引用-》同时查找重写的子类方法
+ */
+class FindMethodUsagesHandler(val methodDef: LuaClassMethodDef) : FindUsagesHandler(methodDef) {
+    override fun getSecondaryElements(): Array<PsiElement> {
+        val arr = arrayListOf<PsiElement>()
+        val query = LuaOverridingMethodsSearch.search(methodDef, true)
+        query.forEach {
+            arr.add(it)
+        }
+        return arr.toTypedArray()
+    }
+
+    override fun findReferencesToHighlight(target: PsiElement, searchScope: SearchScope): MutableCollection<PsiReference> {
+        val collection = super.findReferencesToHighlight(target, searchScope)
+        val query = LuaOverridingMethodsSearch.search(methodDef, true)
+        query.forEach {
+            collection.add(LuaOverridingMethodReference(it, methodDef))
+        }
+        return collection
+    }
+
+    override fun processElementUsages(element: PsiElement, processor: Processor<UsageInfo>, options: FindUsagesOptions): Boolean {
+        if (super.processElementUsages(element, processor, options)) {
+            ApplicationManager.getApplication().runReadAction({
+                val query = LuaOverridingMethodsSearch.search(methodDef, true)
+                query.forEach {
+                    val identifier = it.nameIdentifier
+                    processor.process(UsageInfo(identifier))
+                }
+            })
+        }
+        return true
+    }
+}
