882126bbe1af327dcdf4cd06049ac4a26beea5fd
tangzx <love.tangzx@qq.com>
2017-09-07 12:08:38 +0800

    do not show live template completions in comment

--- src/main/java/com/tang/intellij/lua/codeInsight/LuaCodeTemplateContextType.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/LuaCodeTemplateContextType.kt
@@ -1,6 +1,7 @@
 package com.tang.intellij.lua.codeInsight
 
 import com.intellij.codeInsight.template.TemplateContextType
+import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.util.PsiUtilCore
@@ -17,7 +18,7 @@ class LuaCodeTemplateContextType : TemplateContextType("LUA_CODE", "Lua") {
     override fun isInContext(file: PsiFile, offset: Int): Boolean {
         if (PsiUtilCore.getLanguageAtOffset(file, offset).isKindOf(LuaLanguage.INSTANCE)) {
             val element = file.findElementAt(offset)
-            if (element == null || element is PsiWhiteSpace) {
+            if (element == null || element is PsiWhiteSpace || element is PsiComment) {
                 return false
             }
             if (element.node.elementType in arrayOf(LuaTypes.STRING, LuaTypes.NUMBER)) {
