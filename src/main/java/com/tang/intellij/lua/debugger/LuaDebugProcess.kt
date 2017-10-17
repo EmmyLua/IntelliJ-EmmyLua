179cc5ffeba2b3fbca6a957b7180dfa69fb6aedd
tangzx <love.tangzx@qq.com>
2017-10-17 09:43:30 +0800

    javaj2kt

---
+++ src/main/java/com/tang/intellij/lua/debugger/LuaDebugProcess.kt
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.debugger
+
+import com.intellij.execution.ui.ConsoleViewContentType
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.xdebugger.XDebugProcess
+import com.intellij.xdebugger.XDebugSession
+import com.intellij.xdebugger.XSourcePosition
+import com.intellij.xdebugger.breakpoints.XBreakpointHandler
+import com.intellij.xdebugger.breakpoints.XBreakpointProperties
+import com.intellij.xdebugger.breakpoints.XLineBreakpoint
+import com.intellij.xdebugger.frame.XSuspendContext
+import java.util.concurrent.ConcurrentHashMap
+
+/**
+ *
+ * Created by tangzx on 2017/5/1.
+ */
+abstract class LuaDebugProcess protected constructor(session: XDebugSession) : XDebugProcess(session), DebugLogger {
+    protected var registeredBreakpoints: MutableMap<XSourcePosition, XLineBreakpoint<*>> = ConcurrentHashMap()
+
+    override fun sessionInitialized() {
+        super.sessionInitialized()
+        session.consoleView.addMessageFilter(LuaTracebackFilter(session.project))
+    }
+
+    override fun print(text: String, type: ConsoleViewContentType) {
+        session.consoleView.print(text, type)
+    }
+
+    override fun println(text: String, type: ConsoleViewContentType) {
+        session.consoleView.print(text + "\n", type)
+    }
+
+    override fun error(text: String) {
+        session.consoleView.print(text + "\n", ConsoleViewContentType.ERROR_OUTPUT)
+    }
+
+    override fun resume(context: XSuspendContext?) {
+        run()
+    }
+
+    override fun startStepOut(context: XSuspendContext?) {
+        startStepOver(context)
+    }
+
+    override fun startForceStepInto(context: XSuspendContext?) {
+        startStepInto(context)
+    }
+
+    override fun runToPosition(position: XSourcePosition, context: XSuspendContext?) {
+        resume(context)
+    }
+
+    override fun getBreakpointHandlers(): Array<XBreakpointHandler<*>> {
+        return arrayOf(object : XBreakpointHandler<XLineBreakpoint<XBreakpointProperties<*>>>(LuaLineBreakpointType::class.java) {
+            override fun registerBreakpoint(breakpoint: XLineBreakpoint<XBreakpointProperties<*>>) {
+                val sourcePosition = breakpoint.sourcePosition
+                if (sourcePosition != null) {
+                    this@LuaDebugProcess.registerBreakpoint(sourcePosition, breakpoint)
+                }
+            }
+
+            override fun unregisterBreakpoint(breakpoint: XLineBreakpoint<XBreakpointProperties<*>>, temporary: Boolean) {
+                val sourcePosition = breakpoint.sourcePosition
+                if (sourcePosition != null) {
+                    this@LuaDebugProcess.unregisterBreakpoint(sourcePosition, breakpoint)
+                }
+            }
+        })
+    }
+
+    protected open fun registerBreakpoint(sourcePosition: XSourcePosition, breakpoint: XLineBreakpoint<*>) {
+        registeredBreakpoints.put(sourcePosition, breakpoint)
+    }
+
+    protected open fun unregisterBreakpoint(sourcePosition: XSourcePosition, position: XLineBreakpoint<*>) {
+        registeredBreakpoints.remove(sourcePosition)
+    }
+
+    private fun getBreakpoint(file: VirtualFile, line: Int): XLineBreakpoint<*>? {
+        for (pos in registeredBreakpoints.keys) {
+            if (file == pos.file && line == pos.line) {
+                return registeredBreakpoints[pos]
+            }
+        }
+        return null
+    }
+
+    fun setStack(stack: LuaExecutionStack) {
+        val frames = stack.stackFrames
+        for (topFrame in frames) {
+            val sourcePosition = topFrame.sourcePosition
+            if (sourcePosition != null) {
+                stack.topFrame = topFrame
+                val breakpoint = getBreakpoint(sourcePosition.file, sourcePosition.line)
+                if (breakpoint != null) {
+                    ApplicationManager.getApplication().invokeLater {
+                        session.breakpointReached(breakpoint, null, LuaSuspendContext(stack))
+                        session.showExecutionPoint()
+                    }
+                } else {
+                    ApplicationManager.getApplication().invokeLater {
+                        session.positionReached(LuaSuspendContext(stack))
+                        session.showExecutionPoint()
+                    }
+                }
+                return
+            }
+        }
+
+        // file and source position not found, run it
+        run()
+    }
+
+    protected abstract fun run()
+}
---
+++ src/main/java/com/tang/intellij/lua/debugger/attach/LuaAttachDebugProcess.kt
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.debugger.attach
+
+import com.intellij.execution.ui.ConsoleViewContentType
+import com.intellij.xdebugger.XDebugSession
+import com.intellij.xdebugger.XSourcePosition
+import com.intellij.xdebugger.breakpoints.XLineBreakpoint
+import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider
+import com.intellij.xdebugger.frame.XSuspendContext
+import com.tang.intellij.lua.debugger.LuaDebugProcess
+import com.tang.intellij.lua.debugger.LuaDebuggerEditorsProvider
+import com.tang.intellij.lua.debugger.attach.protos.*
+import com.tang.intellij.lua.psi.LuaFileUtil
+import java.util.concurrent.ConcurrentHashMap
+
+/**
+ *
+ * Created by tangzx on 2017/3/26.
+ */
+abstract class LuaAttachDebugProcess protected constructor(session: XDebugSession) : LuaDebugProcess(session), LuaAttachBridge.ProtoHandler, LuaAttachBridge.ProtoFactory {
+    private val editorsProvider: LuaDebuggerEditorsProvider
+    lateinit var bridge: LuaAttachBridge
+    private val loadedScriptMap = ConcurrentHashMap<Int, LoadedScript>()
+
+    init {
+        session.setPauseActionSupported(false)
+        editorsProvider = LuaDebuggerEditorsProvider()
+    }
+
+    protected abstract fun startBridge(): LuaAttachBridge
+
+    override fun sessionInitialized() {
+        super.sessionInitialized()
+        bridge = startBridge()
+    }
+
+    override fun getEditorsProvider(): XDebuggerEditorsProvider {
+        return editorsProvider
+    }
+
+    override fun startStepOver(context: XSuspendContext?) {
+        bridge.send("stepover")
+    }
+
+    override fun startStepInto(context: XSuspendContext?) {
+        bridge.send("stepinto")
+    }
+
+    override fun startStepOut(context: XSuspendContext?) {
+        bridge.send("stepout")
+    }
+
+    override fun stop() {
+        bridge.stop()
+    }
+
+    override fun run() {
+        bridge.sendRun()
+    }
+
+    override fun handle(proto: LuaAttachProto) {
+        val type = proto.type
+        when (type) {
+            LuaAttachProto.Exception, LuaAttachProto.Message -> {
+                val messageProto = proto as LuaAttachMessageProto
+                messageProto.outputToConsole()
+            }
+            LuaAttachProto.LoadScript -> {
+                val loadScriptProto = proto as LuaAttachLoadScriptProto
+                onLoadScript(loadScriptProto)
+            }
+            LuaAttachProto.Break -> onBreak(proto as LuaAttachBreakProto)
+            LuaAttachProto.SessionEnd, LuaAttachProto.DestroyVM -> {
+                bridge.stop(false)
+                session.stop()
+            }
+        }
+    }
+
+    private fun onBreak(proto: LuaAttachBreakProto) {
+        val file = LuaFileUtil.findFile(session.project, proto.name)
+        if (file == null) {
+            bridge.sendRun()
+            return
+        }
+
+        setStack(proto.stack)
+    }
+
+    private fun onLoadScript(proto: LuaAttachLoadScriptProto) {
+        val file = LuaFileUtil.findFile(session.project, proto.name)
+        if (file == null) {
+            print(String.format("[✘] File not found : %s\n", proto.name), ConsoleViewContentType.SYSTEM_OUTPUT)
+        } else {
+            val script = LoadedScript(file, proto.index, proto.name)
+            loadedScriptMap.put(proto.index, script)
+            print(String.format("[✔] File was loaded : %s\n", proto.name), ConsoleViewContentType.SYSTEM_OUTPUT)
+
+            for (pos in registeredBreakpoints.keys) {
+                if (LuaFileUtil.fileEquals(file, pos.file)) {
+                    val breakpoint = registeredBreakpoints[pos]
+                    bridge.addBreakpoint(proto.index, breakpoint)
+                }
+            }
+        }
+        bridge.sendDone()
+    }
+
+    override fun registerBreakpoint(sourcePosition: XSourcePosition, breakpoint: XLineBreakpoint<*>) {
+        super.registerBreakpoint(sourcePosition, breakpoint)
+        for (script in loadedScriptMap.values) {
+            if (LuaFileUtil.fileEquals(sourcePosition.file, script.file)) {
+                bridge.addBreakpoint(script.index, breakpoint)
+                break
+            }
+        }
+    }
+
+    override fun unregisterBreakpoint(sourcePosition: XSourcePosition, breakpoint: XLineBreakpoint<*>) {
+        super.unregisterBreakpoint(sourcePosition, breakpoint)
+        val sourceFile = sourcePosition.file
+
+        for (script in loadedScriptMap.values) {
+            val scriptFile = script.file
+            if (LuaFileUtil.fileEquals(sourceFile, scriptFile)) {
+                bridge.removeBreakpoint(script.index, breakpoint)
+                break
+            }
+        }
+    }
+
+    fun getScript(index: Int): LoadedScript? {
+        return loadedScriptMap[index]
+    }
+
+    override fun createProto(type: Int): LuaAttachProto {
+        val proto: LuaAttachProto = when (type) {
+            LuaAttachProto.Exception, LuaAttachProto.Message -> LuaAttachMessageProto(type)
+            LuaAttachProto.LoadScript -> LuaAttachLoadScriptProto()
+            LuaAttachProto.SetBreakpoint -> LuaAttachSetBreakpointProto()
+            LuaAttachProto.Break -> LuaAttachBreakProto()
+            LuaAttachProto.EvalResult -> LuaAttachEvalResultProto()
+            else -> LuaAttachProto(type)
+        }
+        proto.process = this
+        return proto
+    }
+}
