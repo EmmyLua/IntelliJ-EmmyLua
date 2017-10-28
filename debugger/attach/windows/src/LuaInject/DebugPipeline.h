fefaa469fa04d2b0caaa5a29b8cc4a373cb219c8
tangzx <love.tangzx@qq.com>
2017-10-28 20:02:16 +0800

    pipeline

---
+++ debugger/attach/windows/src/LuaInject/DebugPipeline.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <string>
+
+class DebugPipeline
+{
+public:
+	virtual ~DebugPipeline() = default;
+	virtual void WriteUInt32(unsigned int value) = 0;
+	virtual void WriteSize(size_t value) = 0;
+	virtual void WriteString(const std::string& value) = 0;
+	virtual void WriteBool(bool value) = 0;
+
+	virtual void ReadUInt32(unsigned int& value) = 0;
+	virtual void ReadSize(size_t& size) = 0;
+	virtual void ReadString(std::string& value) = 0;
+	virtual void ReadBool(bool& value) = 0;
+};
