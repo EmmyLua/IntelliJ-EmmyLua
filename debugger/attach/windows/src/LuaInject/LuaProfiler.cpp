5e7272d90d9d7a74a673e6e207d3b1709783a203
tangzx <love.tangzx@qq.com>
2017-11-03 19:05:04 +0800

    profiler

---
+++ debugger/attach/windows/src/LuaInject/LuaProfiler.cpp
@@ -0,0 +1,5 @@
+﻿#include "LuaProfiler.h"
+
+LPFunctionCall::LPFunctionCall(): id(0), line(0), count(0), time(0)
+{
+}
---
+++ debugger/attach/windows/src/LuaInject/LuaProfiler.h
@@ -0,0 +1,17 @@
+﻿#ifndef _LUA_PROFILER_H_
+#define _LUA_PROFILER_H_
+#include <string>
+
+class LPFunctionCall
+{
+public:
+	LPFunctionCall();
+
+	size_t id;
+	std::string name;
+	size_t line;
+	size_t count;
+	size_t time;
+};
+
+#endif
