ec847aa5198c0c2150a062998fcc8ba60836aa85
tangzx <272669294@qq.com>
2017-11-05 18:26:13 +0800

    clean code

--- debugger/attach/windows/src/LuaInject/DebugPipeline.h
+++ debugger/attach/windows/src/LuaInject/DebugPipeline.h
@@ -9,20 +9,8 @@ class DebugPipeline
 {
 public:
 	virtual ~DebugPipeline() = default;
-	virtual void WriteUInt32(unsigned int value) = 0;
-	virtual void WriteSize(size_t value) = 0;
-	virtual void WriteString(const char* value) = 0;
-	virtual void WriteString(const std::string& value) = 0;
-	virtual void WriteBool(bool value) = 0;
-	virtual void Flush() = 0;
-
-	virtual bool ReadUInt32(unsigned int& value) = 0;
-	virtual bool ReadSize(size_t& size) = 0;
-	virtual bool ReadString(std::string& value) = 0;
-	virtual bool ReadBool(bool& value) = 0;
 
 	virtual bool Send(DebugMessage* message);
-
 	virtual bool Initialize() = 0;
 	virtual void Destroy() = 0;
 	virtual bool IsAttached() = 0;
@@ -36,18 +24,6 @@ public:
 	bool Initialize() override;
 	void Destroy() override;
 	bool IsAttached() override { return true; }
-
-	void WriteUInt32(unsigned value) override { m_eventChannel.WriteUInt32(value); }
-	void WriteSize(size_t value) override { m_eventChannel.WriteSize(value); }
-	void WriteString(const char* value) override { m_eventChannel.WriteString(value); }
-	void WriteString(const std::string& value) override { m_eventChannel.WriteString(value); }
-	void WriteBool(bool value) override { m_eventChannel.WriteBool(value); }
-	void Flush() override { m_eventChannel.Flush(); }
-
-	bool ReadUInt32(unsigned& value) override { return m_commandChannel.ReadUInt32(value); }
-	bool ReadSize(size_t& value) override { return m_commandChannel.ReadSize(value); }
-	bool ReadString(std::string& value) override { return m_commandChannel.ReadString(value); }
-	bool ReadBool(bool& value) override { return m_commandChannel.ReadBool(value); }
 };
 
 class SocketPipeline : public DebugPipeline, public DebugServerListener
@@ -57,20 +33,8 @@ public:
 	bool Initialize() override;
 	void Destroy() override {}
 	bool IsAttached() override;
-
-	void WriteUInt32(unsigned value) override {  }
-	void WriteSize(size_t value) override { }
-	void WriteString(const char* value) override {  }
-	void WriteString(const std::string& value) override { }
-	void WriteBool(bool value) override { }
-	void Flush() override { }
-
-	bool ReadUInt32(unsigned& value) override { return false; }
-	bool ReadSize(size_t& value) override { return false; }
-	bool ReadString(std::string& value) override { return false; }
-	bool ReadBool(bool& value) override { return false; }
+	bool Send(DebugMessage* message) override;
 
 	void onDisconnect(DebugClient* client) override;
 	void handleStream(ByteInputStream* stream) override;
-	bool Send(DebugMessage* message) override;
 };
