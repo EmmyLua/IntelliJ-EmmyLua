dfa3f14224a8aab63cbeb1ade8d76f01bd325b7d
tangzx <love.tangzx@qq.com>
2017-11-09 17:51:37 +0800

    stack node

---
+++ debugger/attach/windows/src/LuaInject/StackNode.cpp
@@ -0,0 +1,42 @@
+﻿#include "StackNode.h"
+#include "Stream.h"
+
+StackNode::StackNode(StackNodeId id): id(id)
+{
+}
+
+void StackNode::Write(ByteOutputStream* stream)
+{
+	stream->WriteByte((char)id);
+	stream->WriteString(key);
+}
+
+void StackNodeContainer::AddChild(StackNode* child)
+{
+	m_children.push_back(child);
+}
+
+StackNodeContainer::~StackNodeContainer()
+{
+	for (auto node : m_children)
+	{
+		delete node;
+	}
+}
+
+void StackNodeContainer::WriteChildren(ByteOutputStream* stream)
+{
+	stream->WriteSize(m_children.size());
+	for (auto node : m_children)
+	{
+		node->Write(stream);
+	}
+}
+
+StackNodeTable::StackNodeTable(): StackNodeContainer(StackNodeId::Table)
+{
+}
+
+StackNodeBinary::StackNodeBinary(): StackNode(StackNodeId::Binary), data(nullptr), size(0)
+{
+}
---
+++ debugger/attach/windows/src/LuaInject/StackNode.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+#include <vector>
+
+class ByteInputStream;
+class ByteOutputStream;
+
+enum class StackNodeId
+{
+	Table,
+	Function,
+	UserData,
+	Binary
+};
+
+class StackNode
+{
+public:
+	StackNode(StackNodeId id);
+	virtual ~StackNode() = default;
+	virtual void Write(ByteOutputStream* stream);
+
+	std::string key;
+private:
+	StackNodeId id;
+};
+
+class StackNodeContainer : StackNode
+{
+public:
+	explicit StackNodeContainer(StackNodeId id)
+		: StackNode(id)
+	{
+	}
+
+	void AddChild(StackNode* child);
+
+	~StackNodeContainer();
+protected:
+	void WriteChildren(ByteOutputStream* stream);
+private:
+	std::vector<StackNode*> m_children;
+};
+
+class StackNodeTable : StackNodeContainer
+{
+public:
+	StackNodeTable();
+};
+
+class StackNodeBinary : StackNode
+{
+public:
+	StackNodeBinary();
+
+	char* data;
+	size_t size;
+};
