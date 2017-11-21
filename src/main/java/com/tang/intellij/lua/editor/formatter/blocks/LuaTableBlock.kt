614d37f7f313b2227087331ba69187dfb22dd7ef
tangzx <love.tangzx@qq.com>
2017-11-21 16:06:24 +0800

    align assign

--- src/main/java/com/tang/intellij/lua/editor/formatter/blocks/LuaTableBlock.kt
+++ src/main/java/com/tang/intellij/lua/editor/formatter/blocks/LuaTableBlock.kt
@@ -24,21 +24,36 @@ import com.intellij.psi.PsiElement
 import com.tang.intellij.lua.editor.formatter.LuaFormatContext
 import com.tang.intellij.lua.psi.LuaTableExpr
 import com.tang.intellij.lua.psi.LuaTableField
+import com.tang.intellij.lua.psi.LuaTypes
 
 class LuaTableBlock(psi: LuaTableExpr, wrap: Wrap?, alignment: Alignment?, indent: Indent, ctx: LuaFormatContext)
     : LuaScriptBlock(psi, wrap, alignment, indent, ctx) {
 
     private val childAlign = Alignment.createAlignment()
 
+    private val assignAlign:Alignment = Alignment.createAlignment(true)
+
     override fun buildChild(child: PsiElement, indent: Indent?): LuaScriptBlock {
         if (child is LuaTableField) {
-            return createBlock(child, Indent.getNormalIndent(), childAlign)
+            return LuaTableFieldBlock(assignAlign, child, null, childAlign, Indent.getNormalIndent(), ctx)
         }
         return super.buildChild(child, indent)
     }
 
-    override fun getChildAttributes(newChildIndex: Int): ChildAttributes {
-        return ChildAttributes(Indent.getNormalIndent(), childAlign)
-        //return super.getChildAttributes(newChildIndex)
+    override fun getChildAttributes(newChildIndex: Int) =
+            ChildAttributes(Indent.getNormalIndent(), childAlign)
+}
+
+class LuaTableFieldBlock(private val assignAlign:Alignment, psi: LuaTableField, wrap: Wrap?, alignment: Alignment?, indent: Indent, ctx: LuaFormatContext)
+    : LuaScriptBlock(psi, wrap, alignment, indent, ctx) {
+
+    override fun buildChild(child: PsiElement, indent: Indent?): LuaScriptBlock {
+        if (ctx.settings.ALIGN_CONSECUTIVE_VARIABLE_DECLARATIONS) {
+            if (child.node.elementType == LuaTypes.ASSIGN) {
+                return createBlock(child, Indent.getNoneIndent(), assignAlign)
+            }
+        }
+        return super.buildChild(child, indent)
     }
+
 }
