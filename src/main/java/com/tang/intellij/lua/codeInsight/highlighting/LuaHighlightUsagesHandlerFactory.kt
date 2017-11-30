5c1b2947d0ddea96bb0e218ecbb1d29e81890f49
tangzx <love.tangzx@qq.com>
2017-11-30 14:27:59 +0800

    highlight unary && binary range

--- src/main/java/com/tang/intellij/lua/codeInsight/highlighting/LuaHighlightUsagesHandlerFactory.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/highlighting/LuaHighlightUsagesHandlerFactory.kt
@@ -30,11 +30,23 @@ import com.tang.intellij.lua.psi.*
  * Created by tangzx on 2017/3/18.
  */
 class LuaHighlightUsagesHandlerFactory : HighlightUsagesHandlerFactoryBase() {
-    val binaryOp = arrayOf(LuaTypes.AND, LuaTypes.OR)
 
     override fun createHighlightUsagesHandler(editor: Editor,
                                               psiFile: PsiFile,
                                               psiElement: PsiElement): HighlightUsagesHandlerBase<*>? {
+        val expr = PsiTreeUtil.getParentOfType(psiElement, LuaUnaryExpr::class.java, LuaBinaryExpr::class.java)
+        if (expr != null) {
+            return object : HighlightUsagesHandlerBase<PsiElement>(editor, psiFile) {
+                override fun selectTargets(list: MutableList<PsiElement>?, consumer: Consumer<MutableList<PsiElement>>?) { }
+
+                override fun computeUsages(list: MutableList<PsiElement>) {
+                    addOccurrence(expr)
+                }
+
+                override fun getTargets() = arrayListOf(psiElement)
+            }
+        }
+
         when(psiElement.node.elementType) {
             LuaTypes.RETURN -> {
                 val returnStat = PsiTreeUtil.getParentOfType(psiElement, LuaReturnStat::class.java)
@@ -52,8 +64,8 @@ class LuaHighlightUsagesHandlerFactory : HighlightUsagesHandlerFactoryBase() {
                     return LoopHandler(editor, psiFile, psiElement, loop)
             }
 
-            in binaryOp -> {
-                val expr = PsiTreeUtil.getParentOfType(psiElement, LuaBinaryExpr::class.java)
+            /*in binaryOp -> {
+                val expr = PsiTreeUtil.getParentOfType(psiElement, LuaExpr::class.java)
                 if (expr != null) {
                     return object : HighlightUsagesHandlerBase<PsiElement>(editor, psiFile) {
                         override fun selectTargets(list: MutableList<PsiElement>?, consumer: Consumer<MutableList<PsiElement>>?) { }
@@ -65,7 +77,7 @@ class LuaHighlightUsagesHandlerFactory : HighlightUsagesHandlerFactoryBase() {
                         override fun getTargets() = arrayListOf(psiElement)
                     }
                 }
-            }
+            }*/
 
             // local a = value
             /*LuaTypes.ID -> {
