8c247282f8348e05fe1ac4b612cad7b834367429
tangzx <272669294@qq.com>
2017-12-31 00:16:15 +0800

    improve performance : folding

--- src/main/java/com/tang/intellij/lua/folding/LuaFoldingBuilder.kt
+++ src/main/java/com/tang/intellij/lua/folding/LuaFoldingBuilder.kt
@@ -17,30 +17,72 @@
 package com.tang.intellij.lua.folding
 
 import com.intellij.lang.ASTNode
-import com.intellij.lang.folding.CustomFoldingBuilder
 import com.intellij.lang.folding.FoldingBuilder
+import com.intellij.lang.folding.FoldingBuilderEx
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.lang.folding.NamedFoldingDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.FoldingGroup
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiRecursiveElementVisitor
 import com.intellij.psi.PsiWhiteSpace
-import com.intellij.psi.util.PsiTreeUtil
+import com.tang.intellij.lua.comment.psi.api.LuaComment
 import com.tang.intellij.lua.psi.LuaBlock
+import com.tang.intellij.lua.psi.LuaTableExpr
 import com.tang.intellij.lua.psi.LuaTypes
 
 /**
  * Created by tangzx
  * Date : 2015/11/16.
  */
-class LuaFoldingBuilder : CustomFoldingBuilder(), FoldingBuilder {
+class LuaFoldingBuilder : FoldingBuilderEx() {
 
-    override fun buildLanguageFoldRegions(list: MutableList<FoldingDescriptor>, psiElement: PsiElement, document: Document, b: Boolean) {
-        collectDescriptorsRecursively(psiElement.node, document, list)
+    override fun buildFoldRegions(element: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+        return if (quick)
+            emptyArray()
+        else {
+            val list = mutableListOf<FoldingDescriptor>()
+            buildLanguageFoldRegions(list, element, document)
+            list.toTypedArray()
+        }
     }
 
-    override fun getLanguagePlaceholderText(astNode: ASTNode, textRange: TextRange): String? {
+    private fun buildLanguageFoldRegions(list: MutableList<FoldingDescriptor>, psiElement: PsiElement, document: Document) {
+        psiElement.accept(object : PsiRecursiveElementVisitor() {
+            override fun visitElement(element: PsiElement) {
+                val node = element.node
+                when(element) {
+                    is LuaBlock -> {
+                        val prev = node.treePrev//element.prevSibling
+                        val next = node.treeNext//element.nextSibling
+                        if (prev != null && next != null) {
+                            val l = prev.startOffset + prev.textLength
+                            val r = next.startOffset
+
+                            val range = TextRange(l, r)
+                            if (range.length > 0 && !addOneLineMethodFolding(list, element, range, prev, next)) {
+                                list.add(FoldingDescriptor(element, range))
+                            }
+                        }
+                    }
+                    is LuaTableExpr -> {
+                        val textRange = node.textRange
+                        if (document.getLineNumber(textRange.startOffset) != document.getLineNumber(textRange.endOffset))
+                            list.add(FoldingDescriptor(node, textRange))
+                    }
+                    is LuaComment -> {
+                        val textRange = node.textRange
+                        if (document.getLineNumber(textRange.startOffset) != document.getLineNumber(textRange.endOffset))
+                            list.add(FoldingDescriptor(node, textRange))
+                    }
+                }
+                super.visitElement(element)
+            }
+        })
+    }
+
+    override fun getPlaceholderText(astNode: ASTNode): String? {
         val type = astNode.elementType
         return when(type) {
             LuaTypes.BLOCK -> HOLDER_TEXT
@@ -50,44 +92,7 @@ class LuaFoldingBuilder : CustomFoldingBuilder(), FoldingBuilder {
         }
     }
 
-    private fun collectDescriptorsRecursively(node: ASTNode,
-                                              document: Document,
-                                              descriptors: MutableList<FoldingDescriptor>) {
-        val type = node.elementType
-        when(type) {
-            LuaTypes.BLOCK -> {
-                val block = node.psi as LuaBlock
-                val prev = PsiTreeUtil.skipSiblingsBackward(block, PsiWhiteSpace::class.java)
-                val next = PsiTreeUtil.skipSiblingsForward(block, PsiWhiteSpace::class.java)
-                if (prev != null && next != null) {
-                    val l = prev.textOffset + prev.textLength
-                    val r = next.textOffset
-
-                    val range = TextRange(l, r)
-                    if (range.length > 0 && !addOneLineMethodFolding(descriptors, block, range, prev, next)) {
-                        descriptors.add(FoldingDescriptor(block, range))
-                    }
-                }
-            }
-            LuaTypes.DOC_COMMENT -> {
-                val textRange = node.textRange
-                if (document.getLineNumber(textRange.startOffset) != document.getLineNumber(textRange.endOffset))
-                    descriptors.add(FoldingDescriptor(node, textRange))
-                return
-            }
-            LuaTypes.TABLE_EXPR -> {
-                val textRange = node.textRange
-                if (document.getLineNumber(textRange.startOffset) != document.getLineNumber(textRange.endOffset))
-                    descriptors.add(FoldingDescriptor(node, textRange))
-            }
-        }
-
-        for (child in node.getChildren(null)) {
-            collectDescriptorsRecursively(child, document, descriptors)
-        }
-    }
-
-    private fun addOneLineMethodFolding(descriptors: MutableList<FoldingDescriptor>, block: LuaBlock, range: TextRange, prev: PsiElement, next: PsiElement): Boolean {
+    private fun addOneLineMethodFolding(descriptors: MutableList<FoldingDescriptor>, block: LuaBlock, range: TextRange, prev: ASTNode, next: ASTNode): Boolean {
         val children = block.children
         if (children.isEmpty()) return false
 
@@ -104,15 +109,15 @@ class LuaFoldingBuilder : CustomFoldingBuilder(), FoldingBuilder {
         if (validCount == 1) {
             if (first!!.textContains('\n')) return false
 
-            val firstRange = first.node.textRange
+            val firstRange = first.textRange
             val lRange = TextRange(range.startOffset, firstRange.startOffset)
             val rRange = TextRange(firstRange.endOffset, range.endOffset)
             if (lRange.isEmpty || rRange.isEmpty)
                 return false
 
             val group = FoldingGroup.newGroup("one-liner")
-            descriptors.add(NamedFoldingDescriptor(prev.node, lRange, group, HOLDER_TEXT))
-            descriptors.add(NamedFoldingDescriptor(next.node, rRange, group, HOLDER_TEXT))
+            descriptors.add(NamedFoldingDescriptor(prev, lRange, group, HOLDER_TEXT))
+            descriptors.add(NamedFoldingDescriptor(next, rRange, group, HOLDER_TEXT))
 
             return true
         }
@@ -120,7 +125,7 @@ class LuaFoldingBuilder : CustomFoldingBuilder(), FoldingBuilder {
         return false
     }
 
-    override fun isRegionCollapsedByDefault(astNode: ASTNode): Boolean {
+    override fun isCollapsedByDefault(astNode: ASTNode): Boolean {
         return astNode.elementType === LuaTypes.REGION
     }
 
