cf022d8496fad746e16f8899e643050ee426f72f
tangzx <272669294@qq.com>
2018-01-04 00:50:34 +0800

    performance : region folding

--- src/main/java/com/tang/intellij/lua/folding/LuaRegionFoldingBuilder.kt
+++ src/main/java/com/tang/intellij/lua/folding/LuaRegionFoldingBuilder.kt
@@ -18,22 +18,41 @@ package com.tang.intellij.lua.folding
 
 import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.CustomFoldingBuilder
+import com.intellij.lang.folding.FoldingBuilderEx
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.tang.intellij.lua.psi.LuaTypes
 
-class LuaRegionFoldingBuilder : CustomFoldingBuilder() {
-    override fun isRegionCollapsedByDefault(astNode: ASTNode): Boolean {
-        return astNode.elementType === LuaTypes.REGION
+class LuaRegionFoldingBuilder : FoldingBuilderEx() {
+
+    private val customFoldingBuilder = object : CustomFoldingBuilder() {
+        override fun isRegionCollapsedByDefault(astNode: ASTNode): Boolean {
+            return astNode.elementType === LuaTypes.REGION
+        }
+
+        override fun buildLanguageFoldRegions(descriptors: MutableList<FoldingDescriptor>, root: PsiElement, document: Document, quick: Boolean) {
+
+        }
+
+        override fun getLanguagePlaceholderText(p0: ASTNode, p1: TextRange): String {
+            return "..."
+        }
     }
 
-    override fun buildLanguageFoldRegions(descriptors: MutableList<FoldingDescriptor>, root: PsiElement, document: Document, quick: Boolean) {
+    override fun getPlaceholderText(astNode: ASTNode) = customFoldingBuilder.getPlaceholderText(astNode)
 
+    override fun getPlaceholderText(node: ASTNode, range: TextRange) = customFoldingBuilder.getPlaceholderText(node, range)
+
+    override fun isCollapsedByDefault(astNode: ASTNode): Boolean {
+        return customFoldingBuilder.isCollapsedByDefault(astNode)
     }
 
-    override fun getLanguagePlaceholderText(p0: ASTNode, p1: TextRange): String {
-        return "..."
+    override fun buildFoldRegions(element: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+        return if (quick)
+            emptyArray()
+        else
+            customFoldingBuilder.buildFoldRegions(element, document, quick)
     }
 }
