4ff0b0d89751b5b93f87f1c2c3f51f74e813853c
tangzx <272669294@qq.com>
2018-01-18 00:22:15 +0800

    optimize postfix : tostring & tonumber

--- src/main/java/com/tang/intellij/lua/codeInsight/postfix/templates/QuickCallPostfix.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/postfix/templates/QuickCallPostfix.kt
@@ -18,11 +18,11 @@ package com.tang.intellij.lua.codeInsight.postfix.templates
 
 import com.intellij.codeInsight.template.postfix.templates.StringBasedPostfixTemplate
 import com.intellij.psi.PsiElement
-import com.tang.intellij.lua.codeInsight.postfix.LuaPostfixUtils.selectorTopmost
+import com.tang.intellij.lua.codeInsight.postfix.LuaPostfixUtils.selectorAllExpressionsWithCurrentOffset
 
-abstract class LuaCallPostfixTemplate(val fn:String) : StringBasedPostfixTemplate(fn, "$fn(expr)", selectorTopmost()) {
+abstract class LuaCallPostfixTemplate(private val fn:String) : StringBasedPostfixTemplate(fn, "$fn(expr)", selectorAllExpressionsWithCurrentOffset()) {
 
-    override fun getTemplateString(psiElement: PsiElement) = "$fn(\$expr$)"
+    override fun getTemplateString(psiElement: PsiElement) = "$fn(\$expr\$)"
 
     override fun getElementToRemove(expr: PsiElement) = expr
 }
