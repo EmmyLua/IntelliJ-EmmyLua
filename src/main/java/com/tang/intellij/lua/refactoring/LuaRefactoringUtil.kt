0af17e07b3f6ab92beace84872a781f696935fbc
tangzx <love.tangzx@qq.com>
2018-01-22 16:58:35 +0800

    name validator for Doc-Class-Name

--- src/main/java/com/tang/intellij/lua/refactoring/LuaRefactoringUtil.kt
+++ src/main/java/com/tang/intellij/lua/refactoring/LuaRefactoringUtil.kt
@@ -18,6 +18,8 @@ package com.tang.intellij.lua.refactoring
 
 import com.intellij.codeInsight.PsiEquivalenceUtil
 import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.util.text.StringUtil.isJavaIdentifierPart
+import com.intellij.openapi.util.text.StringUtil.isJavaIdentifierStart
 import com.intellij.psi.PsiElement
 import com.tang.intellij.lua.psi.LuaVisitor
 import java.util.*
@@ -47,5 +49,22 @@ object LuaRefactoringUtil {
 
     fun isLuaIdentifier(name: String): Boolean {
         return StringUtil.isJavaIdentifier(name)
+    }
+
+    fun isLuaTypeIdentifier(text: String): Boolean {
+        val len = text.length
+        if (len == 0) {
+            return false
+        } else if (!isJavaIdentifierStart(text[0])) {
+            return false
+        } else {
+            for (i in 1 until len) {
+                val c = text[i]
+                if (!isJavaIdentifierPart(c) && c != '.') {
+                    return false
+                }
+            }
+            return true
+        }
     }
 }
---
+++ src/main/java/com/tang/intellij/lua/refactoring/rename/LuaDocClassNameRenameHandler.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.refactoring.rename
+
+import com.intellij.patterns.ElementPattern
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiElement
+import com.intellij.refactoring.rename.RenameInputValidator
+import com.intellij.util.ProcessingContext
+import com.tang.intellij.lua.comment.psi.LuaDocClassDef
+import com.tang.intellij.lua.refactoring.LuaRefactoringUtil
+
+class LuaDocClassRenameInputValidator : RenameInputValidator {
+    override fun isInputValid(newName: String, element: PsiElement, context: ProcessingContext): Boolean {
+        return LuaRefactoringUtil.isLuaTypeIdentifier(newName)
+    }
+
+    override fun getPattern(): ElementPattern<out PsiElement> {
+        return PlatformPatterns.psiElement(LuaDocClassDef::class.java)
+    }
+}
