ccf61170d56e7983abb22f7fba6f7ffa44370e9a
tangzx <272669294@qq.com>
2018-02-11 16:57:28 +0800

    clean code

--- src/main/java/com/tang/intellij/lua/editor/LuaSymbolNavigationContributor.kt
+++ src/main/java/com/tang/intellij/lua/editor/LuaSymbolNavigationContributor.kt
@@ -19,7 +19,6 @@ package com.tang.intellij.lua.editor
 import com.intellij.navigation.ChooseByNameContributor
 import com.intellij.navigation.NavigationItem
 import com.intellij.openapi.project.Project
-import com.intellij.util.containers.HashSet
 import com.tang.intellij.lua.search.SearchContext
 import com.tang.intellij.lua.stubs.index.LuaShortNameIndex
 
@@ -29,7 +28,7 @@ import com.tang.intellij.lua.stubs.index.LuaShortNameIndex
  */
 class LuaSymbolNavigationContributor : ChooseByNameContributor {
     override fun getNames(project: Project, b: Boolean): Array<String> {
-        val nameSet = HashSet<String>()
+        val nameSet = mutableSetOf<String>()
         LuaShortNameIndex.getInstance().processAllKeys(project) { s ->
             nameSet.add(s)
             true
