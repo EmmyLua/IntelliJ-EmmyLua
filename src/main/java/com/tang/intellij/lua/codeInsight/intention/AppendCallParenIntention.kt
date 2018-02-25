1cccb52f8e771b8d9b599b629c9a06bb6326b638
tangzx <272669294@qq.com>
2018-02-25 23:48:16 +0800

    optimize append/remove call paren intention

--- src/main/java/com/tang/intellij/lua/codeInsight/intention/AppendCallParenIntention.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/intention/AppendCallParenIntention.kt
@@ -19,10 +19,10 @@ package com.tang.intellij.lua.codeInsight.intention
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
-import com.intellij.psi.codeStyle.CodeStyleManager
+import com.intellij.psi.util.PsiTreeUtil
 import com.tang.intellij.lua.psi.LuaCallExpr
+import com.tang.intellij.lua.psi.LuaElementFactory
 import com.tang.intellij.lua.psi.LuaPsiTreeUtil
 import com.tang.intellij.lua.psi.LuaTypes
 
@@ -32,23 +32,15 @@ class AppendCallParenIntention : BaseIntentionAction() {
     override fun getText() = familyName
 
     override fun isAvailable(project: Project, editor: Editor, psiFile: PsiFile): Boolean {
-        val callExpr = LuaPsiTreeUtil.findElementOfClassAtOffset(psiFile, editor.caretModel.offset, LuaCallExpr::class.java, false)
-        if (callExpr != null) {
-            val childByType = callExpr.args.node.findChildByType(LuaTypes.LPAREN)
-            return childByType == null
-        }
-        return false
+        val callExpr = LuaPsiTreeUtil.findElementOfClassAtOffset(psiFile, editor.caretModel.offset, LuaCallExpr::class.java, false) ?: return false
+        return callExpr.args.node.findChildByType(LuaTypes.LPAREN) == null
     }
 
     override fun invoke(project: Project, editor: Editor, psiFile: PsiFile) {
-        val callExpr = LuaPsiTreeUtil.findElementOfClassAtOffset(psiFile, editor.caretModel.offset, LuaCallExpr::class.java, false)
-        if (callExpr != null) {
-            val argsNode = callExpr.args.node
-            editor.document.insertString(argsNode.startOffset + argsNode.textLength, ")")
-            editor.document.insertString(argsNode.startOffset, "(")
-
-            PsiDocumentManager.getInstance(project).commitDocument(editor.document)
-            CodeStyleManager.getInstance(project).reformat(callExpr)
-        }
+        val callExpr = LuaPsiTreeUtil.findElementOfClassAtOffset(psiFile, editor.caretModel.offset, LuaCallExpr::class.java, false) ?: return
+        val code = "${callExpr.expr.text}(${callExpr.args.text})"
+        val file = LuaElementFactory.createFile(project, code)
+        val newCall = PsiTreeUtil.findChildOfType(file, LuaCallExpr::class.java) ?: return
+        callExpr.replace(newCall)
     }
 }
--- src/main/java/com/tang/intellij/lua/codeInsight/intention/RemoveCallParenIntention.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/intention/RemoveCallParenIntention.kt
@@ -20,7 +20,7 @@ import com.intellij.codeInsight.intention.impl.BaseIntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
-import com.intellij.psi.codeStyle.CodeStyleManager
+import com.intellij.psi.util.PsiTreeUtil
 import com.tang.intellij.lua.psi.*
 
 class RemoveCallParenIntention : BaseIntentionAction() {
@@ -28,36 +28,32 @@ class RemoveCallParenIntention : BaseIntentionAction() {
 
     override fun getText() = familyName
 
-    override fun isAvailable(project: Project, editor: Editor, psiFile: PsiFile): Boolean {
+    private fun findArgExpr(editor: Editor, psiFile: PsiFile): LuaExpr? {
         val callExpr = LuaPsiTreeUtil.findElementOfClassAtOffset(psiFile, editor.caretModel.offset, LuaCallExpr::class.java, false)
         if (callExpr != null) {
-            val args = callExpr.args as? LuaListArgs ?: return false
+            val args = callExpr.args as? LuaListArgs ?: return null
             val list = args.exprList
             if (list.isEmpty() || list.size > 1)
-                return false
+                return null
             val expr = list.first()
             when (expr) {
-                is LuaTableExpr -> return true
-                is LuaLiteralExpr -> return expr.kind == LuaLiteralKind.String
+                is LuaTableExpr -> return expr
+                is LuaLiteralExpr -> return if (expr.kind == LuaLiteralKind.String) expr else null
             }
         }
-        return false
+        return null
+    }
+
+    override fun isAvailable(project: Project, editor: Editor, psiFile: PsiFile): Boolean {
+        return findArgExpr(editor, psiFile) != null
     }
 
     override fun invoke(project: Project, editor: Editor, psiFile: PsiFile) {
-        val callExpr = LuaPsiTreeUtil.findElementOfClassAtOffset(psiFile, editor.caretModel.offset, LuaCallExpr::class.java, false)
-        if (callExpr != null) {
-            val argsNode = callExpr.args.node
-
-            val rParen = argsNode.findChildByType(LuaTypes.RPAREN)
-            if (rParen != null)
-                argsNode.removeChild(rParen)
-
-            val lParen = argsNode.findChildByType(LuaTypes.LPAREN)
-            if (lParen != null)
-                argsNode.removeChild(lParen)
-
-            CodeStyleManager.getInstance(project).reformat(callExpr)
-        }
+        val callExpr = LuaPsiTreeUtil.findElementOfClassAtOffset(psiFile, editor.caretModel.offset, LuaCallExpr::class.java, false) ?: return
+        val argExpr = findArgExpr(editor, psiFile) ?: return
+        val code = "${callExpr.expr.text} ${argExpr.text}"
+        val file = LuaElementFactory.createFile(project, code)
+        val newCall = PsiTreeUtil.findChildOfType(file, LuaCallExpr::class.java) ?: return
+        callExpr.replace(newCall)
     }
 }
