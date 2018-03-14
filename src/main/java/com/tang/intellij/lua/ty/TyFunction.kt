c39ac0328dc018d773e27e147afb768720d79ebd
tangzx <272669294@qq.com>
2018-03-14 22:32:51 +0800

    disable experimental features

--- src/main/java/com/tang/intellij/lua/ty/TyFunction.kt
+++ src/main/java/com/tang/intellij/lua/ty/TyFunction.kt
@@ -213,11 +213,11 @@ class TyPsiFunction(private val selfCall: Boolean, val psi: LuaFuncBodyOwner, se
            returnTy = Ty.UNKNOWN
         }
 
-        val genericDefList = (psi as? LuaCommentOwner)?.comment?.findTags(LuaDocGenericDef::class.java)
+        /*val genericDefList = (psi as? LuaCommentOwner)?.comment?.findTags(LuaDocGenericDef::class.java)
         val list = mutableListOf<TyParameter>()
-        genericDefList?.forEach { it.name?.let { name -> list.add(TyParameter(name, it.classNameRef?.resolveType())) } }
+        genericDefList?.forEach { it.name?.let { name -> list.add(TyParameter(name, it.classNameRef?.resolveType())) } }*/
 
-        FunSignature(selfCall, returnTy, psi.params, list)
+        FunSignature(selfCall, returnTy, psi.params)
     }
 
     override val signatures: Array<IFunSignature> by lazy {
