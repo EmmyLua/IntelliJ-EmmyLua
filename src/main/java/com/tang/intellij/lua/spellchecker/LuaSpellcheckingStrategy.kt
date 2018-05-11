adc953464919452d91bb09ccbc476214f6f949e7
tangzx <272669294@qq.com>
2018-05-12 00:29:57 +0800

    bug fix spellchecker

--- src/main/java/com/tang/intellij/lua/spellchecker/LuaSpellcheckingStrategy.kt
+++ src/main/java/com/tang/intellij/lua/spellchecker/LuaSpellcheckingStrategy.kt
@@ -17,15 +17,12 @@
 package com.tang.intellij.lua.spellchecker
 
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNameIdentifierOwner
 import com.intellij.spellchecker.inspections.PlainTextSplitter
-import com.intellij.spellchecker.tokenizer.EscapeSequenceTokenizer
-import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy
-import com.intellij.spellchecker.tokenizer.TokenConsumer
-import com.intellij.spellchecker.tokenizer.Tokenizer
+import com.intellij.spellchecker.tokenizer.*
 import com.tang.intellij.lua.lang.LuaLanguage
 import com.tang.intellij.lua.psi.LuaLiteralExpr
 import com.tang.intellij.lua.psi.LuaLiteralKind
-import com.tang.intellij.lua.psi.LuaTypes
 import com.tang.intellij.lua.psi.kind
 
 class LuaSpellcheckingStrategy : SpellcheckingStrategy() {
@@ -33,14 +30,24 @@ class LuaSpellcheckingStrategy : SpellcheckingStrategy() {
         return element.language is LuaLanguage
     }
 
-    override fun getTokenizer(element: PsiElement?): Tokenizer<*> {
-        if (element?.node?.elementType == LuaTypes.LITERAL_EXPR)
-            return StringLiteralTokenizer
-        return super.getTokenizer(element)
+    override fun getTokenizer(element: PsiElement): Tokenizer<*> {
+        return when (element) {
+            is LuaLiteralExpr -> StringLiteralTokenizer
+            is PsiNameIdentifierOwner -> IdentifierOwnerTokenizer
+            else -> super.getTokenizer(element)
+        }
     }
 }
 
-object StringLiteralTokenizer : EscapeSequenceTokenizer<LuaLiteralExpr>() {
+private object IdentifierOwnerTokenizer : PsiIdentifierOwnerTokenizer() {
+    override fun tokenize(element: PsiNameIdentifierOwner, consumer: TokenConsumer) {
+        val id = element.nameIdentifier
+        if (id !is LuaLiteralExpr)
+            super.tokenize(element, consumer)
+    }
+}
+
+private object StringLiteralTokenizer : EscapeSequenceTokenizer<LuaLiteralExpr>() {
 
     override fun tokenize(element: LuaLiteralExpr, consumer: TokenConsumer) {
         if (element.kind == LuaLiteralKind.String) {
