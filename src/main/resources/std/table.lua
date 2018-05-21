2f8d10017225460a0f0deda19d80ac0732f8a32a
dsmgit <39238275+dsmgit@users.noreply.github.com>
2018-05-21 11:21:00 +0200

    fix optional params

--- src/main/resources/std/table.lua
+++ src/main/resources/std/table.lua
@@ -19,11 +19,11 @@ table = {}
 --- `list[i]..sep..list[i+1] ... sep..list[j]`. The default value for
 --- `sep` is the empty string, the default for `i` is 1, and the default for
 --- `j` is #list. If `i` is greater than `j`, returns the empty string.
----@overload fun(t:table, sep:string):string
+---@overload fun(list:table):string
 ---@param list table
----@param optional sep string
----@param optional i number
----@param optional j number
+---@param sep string
+---@param i number
+---@param j number
 ---@return string
 function table.concat(list, sep, i, j) end
 
@@ -32,9 +32,9 @@ function table.concat(list, sep, i, j) end
 --- elements to `list[pos]`, `list[pos+1]`, `···`, `list[#list]`. The default
 --- value for `pos` is ``#list+1`, so that a call `table.insert(t,x)`` inserts
 --- `x` at the end of list `t`.
----@overload fun(t:table, pos:number, value:any):number
+---@overload fun(list:table, value:any):number
 ---@param list table
----@param optional pos number
+---@param pos number
 ---@param value any
 ---@return number
 function table.insert(list, pos, value) end
@@ -46,11 +46,12 @@ function table.insert(list, pos, value) end
 --- range. The number of elements to be moved must fit in a Lua integer.
 ---
 --- Returns the destination table `a2`.
+---@overload fun(a1:table, f:number, e:number, t:number):table
 ---@param a1 table
 ---@param f number
 ---@param e number
 ---@param t number
----@param optional a2 table
+---@param a2 table
 ---@return table
 function table.move(a1, f, e, t, a2) end
 
@@ -59,48 +60,53 @@ function table.move(a1, f, e, t, a2) end
 --- with a field "`n`" with the total number of arguments. Note that the
 --- resulting table may not be a sequence, if some arguments are **nil**.
 ---@return table
-function table.pack(···) end
+function table.pack(...) end
 
 ---
---- Removes from `table` the element at position `pos`, shifting down other
---- elements to close the space, if necessary. Returns the value of the removed
---- element. The default value for `pos` is `n`, where `n` is the length of the
---- table, so that a call `table.remove(t)` removes the last element of table
---- `t`.
----@overload fun(t:table, pos:number):any
----@param t table
----@param optional pos number
+--- Removes from `list` the element at position `pos`, returning the value of
+--- the removed element. When `pos` is an integer between 1 and `#list`, it
+--- shifts down the elements `list[pos+1]`, `list[pos+2]`, `···`,
+--- `list[#list]` and erases element `list[#list]`; The index pos can also be 0
+--- when `#list` is 0, or `#list` + 1; in those cases, the function erases
+--- the element `list[pos]`.
+---
+--- The default value for `pos` is `#list`, so that a call `table.remove(l)`
+--- removes the last element of list `l`.
+---@overload fun(list:table):any
+---@param list table
+---@param pos number
 ---@return any
-function table.remove(t, pos) end
+function table.remove(list, pos) end
 
 ---
---- Sorts table elements in a given order, *in-place*, from `table[1]` to
---- `table[n]`, where `n` is the length of the table. If `comp` is given,
---- then it must be a function that receives two table elements, and returns
---- true when the first is less than the second in the final order  (so that,
---- after the `sort`, `i < j` implies `not comp(a[i+1], a[i])`. If comp is
---- not given, then the standard Lua operator `<` is used instead.
+--- Sorts list elements in a given order, *in-place*, from `list[1]` to
+--- `list[#list]`. If `comp` is given, then it must be a function that receives
+--- two list elements and returns true when the first element must come before
+--- the second in the final order (so that, after the sort, `i < j` implies not
+--- `comp(list[j],list[i]))`. If `comp` is not given, then the standard Lua
+--- operator `<` is used instead.
 ---
---- Note that the comp function must define a strict partial order over the
+--- Note that the `comp` function must define a strict partial order over the
 --- elements in the list; that is, it must be asymmetric and transitive.
 --- Otherwise, no valid sort may be possible.
 ---
 --- The sort algorithm is not stable: elements considered equal by the given
 --- order may have their relative positions changed by the sort.
----@param t table
----@param optional comp fun(a:any, b:any):number
+---@overload fun(list:table):number
+---@param list table
+---@param comp fun(i:any, j:any):number
 ---@return number
-function table.sort(t, comp) end
+function table.sort(list, comp) end
 
 ---
 --- Returns the elements from the given list. This function is equivalent to
----
 --- return `list[i]`, `list[i+1]`, `···`, `list[j]`
 --- By default, i is 1 and j is #list.
+---@overload fun(list:table):any
 ---@param list table
----@param optional i number
----@param optional j number
----@return number
+---@param i number
+---@param j number
+---@return any
 function table.unpack(list, i, j) end
 
 return table
