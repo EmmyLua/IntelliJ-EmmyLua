b79194c4fcbe8b321d8e8e90453d1187c10168d4
tangzx <272669294@qq.com>
2019-01-27 15:29:17 +0800

    add test case

---
+++ src/test/kotlin/com/tang/intellij/test/completion/TestStringLiteralType.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.test.completion
+
+class TestStringLiteralType : TestCompletionBase() {
+    fun `test literal type 1`() {
+        doTest("""
+            --- test_tuple_1.lua
+
+            ---@param type 'A' | "B"
+            local function addListener(type, func)
+            end
+
+            addListener(--[[caret]]
+        """) {
+            assertTrue(it.containsAll(listOf("A", "B")))
+        }
+    }
+}
