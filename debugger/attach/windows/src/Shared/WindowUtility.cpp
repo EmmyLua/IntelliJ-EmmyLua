3c25b0f097e4e93df33f6955e248cf4c856d6327
shenfang <shennfang@gmail.com>
2019-04-24 17:34:00 +0800

    ignore some not need process in process list

--- debugger/attach/windows/src/Shared/WindowUtility.cpp
+++ debugger/attach/windows/src/Shared/WindowUtility.cpp
@@ -2,6 +2,7 @@
 #include <tlhelp32.h>
 #include <psapi.h>
 #include <shlobj.h>
+#include <algorithm>
 
 HWND GetProcessWindow(DWORD processId)
 {
@@ -33,6 +34,34 @@ HWND GetProcessWindow(DWORD processId)
 
 }
 
+inline char char_tolower(char  c) {
+	return (char)tolower(c);
+}
+
+inline char char_toupper(char  c) {
+	return (char)toupper(c);
+}
+
+
+static void EmmyToLowerCase(std::string& str)
+{
+	std::transform(
+		str.begin(),
+		str.end(),
+		str.begin(),
+		char_tolower);
+}
+
+//-----------------------------------------------------------------------
+static void EmmyToUpperCase(std::string& str)
+{
+	std::transform(
+		str.begin(),
+		str.end(),
+		str.begin(),
+		char_toupper);
+}
+
 void GetProcesses(std::vector<Process>& processes)
 {
 	static char fileName[_MAX_PATH];
@@ -50,11 +79,21 @@ void GetProcesses(std::vector<Process>& processes)
 
 		if (Process32First(snapshot, &processEntry))
 		{
+			char windowsPath[MAX_PATH] = { 0 };
+			bool isGetWindowsPath = false;
+			std::string strWinPath;
+			if (SHGetFolderPath(nullptr, CSIDL_WINDOWS, nullptr, SHGFP_TYPE_CURRENT, windowsPath) == 0)
+			{
+				strWinPath = windowsPath;
+				EmmyToLowerCase(strWinPath);
+				isGetWindowsPath = true;
+			}
+
+
 			do
 			{
 				if (processEntry.th32ProcessID != currentProcessId && processEntry.th32ProcessID != 0)
 				{
-
 					Process process;
 
 					process.id = processEntry.th32ProcessID;
@@ -67,22 +106,62 @@ void GetProcesses(std::vector<Process>& processes)
 							process.path = "error";
 						else process.path = fileName;
 
-						if (!process.path.empty()) {
-							char windowsPath[MAX_PATH];
-							if (SHGetFolderPath(nullptr, CSIDL_WINDOWS, nullptr, SHGFP_TYPE_CURRENT, windowsPath) == 0) {
-								if (process.path.find(windowsPath) == std::string::npos) {
+						EmmyToLowerCase(process.path);
 
-									HWND hWnd = GetProcessWindow(processEntry.th32ProcessID);
+						if (!process.path.empty())
+						{
+							if (isGetWindowsPath &&  process.path.find(strWinPath.c_str()) != std::string::npos)
+							{
+								//on windows path exe
+								continue;
+							}
 
-									if (hWnd != nullptr)
-									{
-										char buffer[1024];
-										GetWindowText(hWnd, buffer, 1024);
-										process.title = buffer;
-									}
-									processes.push_back(process);
+							std::string skipExeNameList[] = {
+								"360se",
+								"MSBuild",
+								"vcpkgsrv",
+								"ServiceHub",
+								"VcxprojReader",
+								"mspdbsrv",
+								"TGitCache",
+								"TortoiseGitProc",
+								"devenv.exe",
+								"PerfWatson2",
+								"TSVNCache",
+								"steamwebhelper",
+								"UnrealCEFSubProcess",
+								"Microsoft.",
+								"VaCodeInspectionsServer",
+								"Steam.exe",
+							};
+
+							
+							size_t totalSkip = sizeof(skipExeNameList) / sizeof(skipExeNameList[0]);
+							bool needSkip = false;
+							for (int i = 0; i < totalSkip; i++)
+							{
+								EmmyToLowerCase(skipExeNameList[i]);
+								needSkip = process.path.find(skipExeNameList[i].c_str()) != std::string::npos;
+								if (needSkip)
+								{
+									break;
 								}
 							}
+
+							if (needSkip)
+							{
+								continue;
+							}
+
+							HWND hWnd = GetProcessWindow(processEntry.th32ProcessID);
+
+							if (hWnd != nullptr)
+							{
+								char buffer[1024];
+								GetWindowText(hWnd, buffer, 1024);
+								process.title = buffer;
+							}
+							processes.push_back(process);
 						}
 					}
 				}
