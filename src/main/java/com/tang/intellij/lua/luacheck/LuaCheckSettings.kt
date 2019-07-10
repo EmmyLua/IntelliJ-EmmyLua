13e13b7c4075fee74a564dd3f3f63339822c9866
tangzx <272669294@qq.com>
2019-07-11 00:13:02 +0800

    clean code

--- src/main/java/com/tang/intellij/lua/luacheck/LuaCheckSettings.kt
+++ src/main/java/com/tang/intellij/lua/luacheck/LuaCheckSettings.kt
@@ -23,7 +23,7 @@ import com.intellij.openapi.components.Storage
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.xmlb.XmlSerializerUtil
 
-@State(name = "LuaCheckSettings", storages = arrayOf(Storage("emmy.xml")))
+@State(name = "LuaCheckSettings", storages = [Storage("emmy.xml")])
 class LuaCheckSettings : PersistentStateComponent<LuaCheckSettings> {
     var luaCheck:String? = null
     var luaCheckArgs:String? = null
