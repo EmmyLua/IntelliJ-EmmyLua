121005d363a723a4468a30e008b70f8bb4b4d942
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2020-02-17 04:13:35 +1100

    Fixed assign type inspection with multiple return values / tuples

--- src/main/java/com/tang/intellij/lua/codeInsight/inspection/AssignTypeInspection.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/inspection/AssignTypeInspection.kt
@@ -17,6 +17,7 @@
 package com.tang.intellij.lua.codeInsight.inspection
 
 import com.intellij.codeInspection.LocalInspectionToolSession
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
 import com.tang.intellij.lua.psi.LuaAssignStat
@@ -24,10 +25,7 @@ import com.tang.intellij.lua.psi.LuaIndexExpr
 import com.tang.intellij.lua.psi.LuaTableExpr
 import com.tang.intellij.lua.psi.LuaVisitor
 import com.tang.intellij.lua.search.SearchContext
-import com.tang.intellij.lua.ty.Ty
-import com.tang.intellij.lua.ty.TyClass
-import com.tang.intellij.lua.ty.TyGeneric
-import com.tang.intellij.lua.ty.TyVarianceFlags
+import com.tang.intellij.lua.ty.*
 
 class AssignTypeInspection : StrictInspection() {
     override fun buildVisitor(myHolder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession): PsiElementVisitor =
@@ -36,47 +34,59 @@ class AssignTypeInspection : StrictInspection() {
                     super.visitAssignStat(o)
 
                     val assignees = o.varExprList.exprList
-                    val values = o.valueExprList?.exprList ?: listOf()
+                    val expressions = o.valueExprList?.exprList ?: listOf()
                     val searchContext = SearchContext.get(o)
 
-                    // Check right number of fields/assignments
-                    if (assignees.size > values.size) {
-                        for (i in values.size until assignees.size) {
-                            myHolder.registerProblem(assignees[i], "Missing value assignment.")
-                        }
-                    } else if (assignees.size < values.size) {
-                        for (i in assignees.size until values.size) {
-                            myHolder.registerProblem(values[i], "Nothing to assign to.")
-                        }
-                    } else {
-                        // Try to match types for each assignment
-                        for (i in 0 until assignees.size) {
-                            val field = assignees[i]
-                            val name = field.name ?: ""
-                            val value = values[i]
-                            val valueType = value.guessType(searchContext)
-                            val varianceFlags = if (value is LuaTableExpr) TyVarianceFlags.WIDEN_TABLES else 0
+                    var assigneeIndex = 0
+
+                    for (expressionIndex in 0 until expressions.size) {
+                        val expression = expressions[expressionIndex]
+                        val expressionType = expression.guessType(searchContext)
+                        val varianceFlags = if (expression is LuaTableExpr) TyVarianceFlags.WIDEN_TABLES else 0
+                        val values = if (expressionType is TyTuple) expressionType.list else listOf(expressionType)
+                        val isLastExpression = expressionIndex == expressions.size - 1
+
+                        for (value in values) {
+                            if (assigneeIndex >= assignees.size) {
+                                for (i in expressionIndex until expressions.size) {
+                                    myHolder.registerProblem(expressions[i], "Insufficient assignees, values will be discarded.", ProblemHighlightType.WEAK_WARNING)
+                                }
+                                return
+                            }
+
+                            val assignee = assignees[assigneeIndex++]
 
                             // Field access
-                            if (field is LuaIndexExpr) {
+                            if (assignee is LuaIndexExpr) {
                                 // Get owner class
-                                val fieldOwnerType = field.guessParentType(searchContext)
+                                val fieldOwnerType = assignee.guessParentType(searchContext)
                                 val fieldOwnerClass = if (fieldOwnerType is TyGeneric) fieldOwnerType.base else fieldOwnerType
 
                                 if (fieldOwnerClass is TyClass) {
+                                    val name = assignee.name ?: ""
                                     val fieldType = fieldOwnerClass.findMemberType(name, searchContext) ?: Ty.NIL
 
-                                    if (!fieldType.contravariantOf(valueType, searchContext, varianceFlags)) {
-                                        myHolder.registerProblem(value, "Type mismatch. Required: '%s' Found: '%s'".format(fieldType, valueType))
+                                    if (!fieldType.contravariantOf(value, searchContext, varianceFlags)) {
+                                        myHolder.registerProblem(expression, "Type mismatch. Required: '%s' Found: '%s'".format(fieldType, value))
                                     }
                                 }
                             } else {
-                                val fieldType = field.guessType(searchContext)
+                                val fieldType = assignee.guessType(searchContext)
 
-                                if (!fieldType.contravariantOf(valueType, searchContext, varianceFlags)) {
-                                    myHolder.registerProblem(value, "Type mismatch. Required: '%s' Found: '%s'".format(fieldType, valueType))
+                                if (!fieldType.contravariantOf(value, searchContext, varianceFlags)) {
+                                    myHolder.registerProblem(expression, "Type mismatch. Required: '%s' Found: '%s'".format(fieldType, value))
                                 }
                             }
+
+                            if (!isLastExpression) {
+                                break // Multiple (tuple) values are only handled for the last expression
+                            }
+                        }
+                    }
+
+                    if (assigneeIndex < assignees.size) {
+                        for (i in assigneeIndex until assignees.size) {
+                            myHolder.registerProblem(assignees[i], "Too many assignees, will be assigned nil.")
                         }
                     }
                 }
