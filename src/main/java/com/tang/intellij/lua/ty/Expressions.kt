19a568575bb188d38ee49c41a1ed518be1edf9d9
tangzx <272669294@qq.com>
2020-10-05 17:13:25 +0800

    bug fix #359

--- src/main/java/com/tang/intellij/lua/ty/Expressions.kt
+++ src/main/java/com/tang/intellij/lua/ty/Expressions.kt
@@ -359,10 +359,14 @@ private fun LuaIndexExpr.infer(context: SearchContext): ITy {
         val propName = indexExpr.name
         if (propName != null) {
             val prefixType = parentTy ?: indexExpr.guessParentType(context)
+            prefixType.eachTopClass(Processor { clazz ->
+                result = result.union(guessFieldType(propName, clazz, context))
+                true
+            })
+            
+            // table<string, K> -> member type is K
             prefixType.each { ty ->
-                if (ty is ITyClass)
-                    result = result.union(guessFieldType(propName, ty, context))
-                else if (ty is ITyGeneric && ty.getParamTy(0) == Ty.STRING)
+                if (ty is ITyGeneric && ty.getParamTy(0) == Ty.STRING)
                     result = result.union(ty.getParamTy(1))
             }
         }
