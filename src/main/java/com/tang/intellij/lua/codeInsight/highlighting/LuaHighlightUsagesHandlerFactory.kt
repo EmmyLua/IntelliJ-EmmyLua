4e553e2b62af19672175ef791a5eac746af56f59
tangzx <272669294@qq.com>
2020-10-08 21:02:48 +0800

    fix highlight issue since IDEA.202 由于新版本IDEA 202采用了新的引用高亮算法，导致Lua高亮失效，这里自己处理高亮

--- src/main/java/com/tang/intellij/lua/codeInsight/highlighting/LuaHighlightUsagesHandlerFactory.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/highlighting/LuaHighlightUsagesHandlerFactory.kt
@@ -18,11 +18,17 @@ package com.tang.intellij.lua.codeInsight.highlighting
 
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerFactoryBase
+import com.intellij.codeInsight.highlighting.ReadWriteAccessDetector
+import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiNameIdentifierOwner
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.Consumer
+import com.tang.intellij.lua.codeInsight.LuaReadWriteAccessDetector
 import com.tang.intellij.lua.psi.*
 
 /**
@@ -66,10 +72,50 @@ class LuaHighlightUsagesHandlerFactory : HighlightUsagesHandlerFactoryBase() {
                         override fun getTargets() = arrayListOf(psiElement)
                     }
                 }
+                else if (parent is LuaNameExpr || parent is LuaIndexExpr) {
+                    return ReferenceHighlightHandler(editor, psiFile, parent)
+                }
             }
         }
         return null
     }
+}
+
+/**
+ * 由于新版本IDEA 202采用了新的Reference高亮算法，导致Lua高亮失效，这里自己处理高亮
+ */
+private class ReferenceHighlightHandler(editor: Editor, psiFile: PsiFile, val psi:PsiElement) : HighlightUsagesHandlerBase<PsiElement>(editor, psiFile) {
+
+    private val list: List<PsiElement> by lazy {
+        val result = mutableListOf<PsiElement>()
+        val ref = psi.reference?.resolve() ?: psi
+        if (ref.containingFile == psiFile)
+            result.add(ref)
+        val query = ReferencesSearch.search(ref, GlobalSearchScope.fileScope(psiFile))
+        query.forEach {
+            result.add(it.element)
+        }
+        result
+    }
+
+    override fun getTargets() = arrayListOf(psi)
+
+    override fun selectTargets(list: MutableList<out PsiElement>, consumer: Consumer<in MutableList<out PsiElement>>) = Unit
+
+    override fun computeUsages(list: MutableList<out PsiElement>) {
+        val detector = LuaReadWriteAccessDetector()
+        this.list.forEach { psi ->
+            val rangeElement = (psi as? PsiNameIdentifierOwner)?.nameIdentifier ?: psi
+            val range = InjectedLanguageManager.getInstance(this.psi.project).injectedToHost(rangeElement, rangeElement.textRange)
+            val access = detector.getExpressionAccess(psi)
+            if (access == ReadWriteAccessDetector.Access.Read)
+                myReadUsages.add(range)
+            else
+                myWriteUsages.add(range)
+        }
+    }
+
+    override fun highlightReferences() = false
 }
 
 private class LoopHandler(editor: Editor, psiFile: PsiFile, val psi:PsiElement, val loop: LuaLoop) : HighlightUsagesHandlerBase<PsiElement>(editor, psiFile) {
