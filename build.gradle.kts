b5a5e2ae0d6ce3b95420254aeab1120201bcac47
tangzx <272669294@qq.com>
2022-02-13 10:01:15 +0800

    upgrade intellij gradle plugin to 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ import org.apache.tools.ant.taskdefs.condition.Os
 import java.io.ByteArrayOutputStream
 
 plugins {
-    id("org.jetbrains.intellij").version("1.3.0")
+    id("org.jetbrains.intellij").version("1.4.0")
     id("org.jetbrains.kotlin.jvm").version("1.4.20")
     id("de.undercouch.download").version("3.4.3")
 }
