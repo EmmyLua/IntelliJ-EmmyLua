96b48ee21972ddcea292440a7a70efc607ae9e63
Петросян Александр Фридрихович <afpetrosyan@beeline.ru>
2022-11-01 18:13:20 +0300

    fixing https://github.com/neopaf/IntelliJ-EmmyLua/issues/1

--- src/main/java/com/tang/intellij/lua/debugger/LuaTracebackFilter.kt
+++ src/main/java/com/tang/intellij/lua/debugger/LuaTracebackFilter.kt
@@ -35,17 +35,17 @@ class LuaTracebackFilter(private val project: Project) : Filter {
         //Test.lua:7: in function 'b'
         //Test.lua:11: in main chunk
 
-        val pattern = Pattern.compile("\\s*((\\.\\.\\.)?(/+)?[^<>\\\\|:\"*? ]+):(\\d+):")
+        val pattern = Pattern.compile("\\s*((/+)?[^<>\\\\|:\"*? ]+):(\\d+):")
         val matcher = pattern.matcher(line)
         if (matcher.find()) {
-            val fileName = matcher.group(1)
-            val lineNumber = Integer.parseInt(matcher.group(4))
+            val fileName = matcher.group(1).replace("^\\.\\.\\.", "") // remove leading '...', it confuses finders
+            val lineNumber = Integer.parseInt(matcher.group(3))
             val file = LuaFileUtil.findFile(project, fileName)
             if (file != null) {
                 val hyperlink = OpenFileHyperlinkInfo(project, file, lineNumber - 1)
                 val textStartOffset = entireLength - line.length
                 val startPos = matcher.start(1)
-                val endPos = matcher.end(4)
+                val endPos = matcher.end(3) + 1
                 return Filter.Result(startPos + textStartOffset, endPos + textStartOffset, hyperlink)
             }
         }
