22229615fddc3050da9a2165c80d84866efc3485
tangzx <272669294@qq.com>
2023-03-12 15:16:59 +0800

    fix deprecated methods in class LuaFileTypeDetector

--- src/main/java/com/tang/intellij/lua/lang/LuaFileTypeDetector.kt
+++ src/main/java/com/tang/intellij/lua/lang/LuaFileTypeDetector.kt
@@ -23,7 +23,6 @@ import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFile
 
 class LuaFileTypeDetector : FileTypeRegistry.FileTypeDetector {
-    override fun getVersion() = 1
 
     override fun detect(file: VirtualFile, firstBytes: ByteSequence, firstCharsIfText: CharSequence?): FileType? {
         return if (FileUtil.isHashBangLine(firstCharsIfText, "lua")) LuaFileType.INSTANCE else null
